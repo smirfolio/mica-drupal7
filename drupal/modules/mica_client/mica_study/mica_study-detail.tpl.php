35c971d563b25938e27bcd5703c4584e0b45eb40
samir <ben.sghaier.samir@gmail.com>
2014-06-03 10:36:53 -0400

    Pull request #3 Typo commons ok

--- drupal/modules/mica_client/mica_study/mica_study-detail.tpl.php
+++ drupal/modules/mica_client/mica_study/mica_study-detail.tpl.php
@@ -17,8 +17,7 @@
         <div class="field-items">
             <div class="field-item even" property="content:encoded">
                 <p>
-                    <?php  print !empty($context_detail['data']->objectives) ?
-                        mica_commons_get_localized_field($context_detail['data'], 'objectives') : ''; ?>
+                    <?php print mica_commons_get_localized_field($context_detail['data'], 'objectives'); ?>
                 </p>
             </div>
         </div>
@@ -30,23 +29,32 @@
     <h2><?php print t('General Information') ?></h2>
 
     <div>
+
+        <?php if(!empty($context_detail['data']->acronym)): ?>
         <div class="field field-name-field-acroym field-type-text field-label-inline clearfix">
             <div class="field-label"><?php print t('Acronym') ?> :</div>
             <div class="field-items">
                 <div class="field-item even">
-                    <?php print !empty($context_detail['data']->acronym) ? mica_commons_get_localized_field($context_detail['data'], 'acronym') : ''; ?>
+                    <?php print mica_commons_get_localized_field($context_detail['data'], 'acronym'); ?>
                 </div>
             </div>
         </div>
+        <?php endif; ?>
+
+        <?php if(!empty($context_detail['data']->website)): ?>
         <div class="field field-name-field-website field-type-link-field field-label-inline clearfix">
             <div class="field-label"><?php print t('Website') ?> :</div>
             <div class="field-items">
-                <div class="field-item even"><a href="http://www.adoquest.ca" target="_blank">
-                        <?php print !empty($context_detail['data']->website) ? $context_detail['data']->website : ''; ?>
+                <div class="field-item even"><a href=" <?php print $context_detail['data']->website; ?>"
+                                                target="_blank">
+                        <?php print $context_detail['data']->website; ?>
                     </a>
                 </div>
             </div>
         </div>
+        <?php endif; ?>
+
+        <?php if(!empty($context_detail['data']->investigators)): ?>
         <div class="field field-name-field-investigators field-type-node-reference field-label-inline clearfix">
             <div class="field-label"><?php print t('Investigators') ?> :</div>
             <?php foreach ($context_detail['data']->investigators as $investigator) : ?>
@@ -60,6 +68,9 @@
                 </div>
             <?php endforeach; ?>
         </div>
+        <?php endif; ?>
+
+        <?php if(!empty($context_detail['data']->contacts)): ?>
         <div class="field field-name-field-contacts-ref field-type-node-reference field-label-inline clearfix">
             <div class="field-label"><?php print t('Contacts') ?> :</div>
             <div class="field-items">
@@ -75,25 +86,144 @@
                 <?php endforeach; ?>
             </div>
         </div>
+        <?php endif; ?>
+
+        <?php if(!empty($context_detail['data']->startYear)): ?>
         <div class="field field-name-field-study-start-year field-type-number-integer field-label-inline clearfix">
             <div class="field-label"><?php print t('Study Start Year') ?> :</div>
             <div class="field-items">
                 <div
-                    class="field-item even"><?php print !empty($context_detail['data']->startYear) ? $context_detail['data']->startYear : ''; ?></div>
+                    class="field-item even"><?php print $context_detail['data']->startYear; ?></div>
             </div>
         </div>
-        <div class="field field-name-field-networks field-type-node-reference field-label-inline clearfix">
+        <?php endif; ?>
+
+        <?php if(!empty($context_detail['data']->endYear)): ?>
+            <div class="field field-name-field-study-end-year field-type-number-integer field-label-inline clearfix">
+                <div class="field-label"><?php print t('Study End Year') ?> :</div>
+                <div class="field-items">
+                    <div
+                        class="field-item even"><?php print $context_detail['data']->endYear; ?></div>
+                </div>
+            </div>
+        <?php endif; ?>
+        <!--
+        <?php if(!empty($context_detail['data']->networks)): ?>
+        <div class="field field-name-field-networks field-type-node-reference field-label-inline clearfix hide">
             <div class="field-label"><?php print t('Networks') ?> :</div>
             <div class="field-items">
                 <div class="field-item even">
-                    <a
-                        href=""><?php print !empty($context_detail['data']->networks) ? $context_detail['data']->networks : ''; ?></a>
+                    <a href=""><?php //print $context_detail['data']->networks; ?></a>
                 </div>
             </div>
         </div>
+        <?php endif; ?>
+        -->
     </div>
 
 </section>
 <section>
     <h2><?php print t('General Design') ?></h2>
+
+    <div>
+
+        <?php if(!empty($context_detail['data']->methods->designs)): ?>
+        <div class="field field-name-field-design field-type-list-text field-label-inline clearfix">
+            <div class="field-label">
+                <?php print t('Study design') ?> :
+            </div>
+            <div class="field-items">
+                <?php foreach ($context_detail['data']->methods->designs as $design): ?>
+                    <div class="field-item">
+                        <?php if ($design == 'other'): ?>
+                            <div class="field-label">
+                                <?php print $design ?> :
+                            </div>
+                            <span class="other-sub-value">
+                                     <?php print mica_commons_get_localized_field($context_detail['data']->methods, 'otherDesign'); ?>
+                            </span>
+                        <?php else: ?>
+                            <?php print $design; ?>
+                        <?php endif; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <?php endif; ?>
+
+        <?php if(!empty($context_detail['data']->methods->followUpInfo)): ?>
+        <div class="field field-name-field-info-design-follow-up field-type-text-long field-label-inline clearfix">
+            <div class="field-label">
+                <?php print t('General Information on Follow Up (profile and frequency)') ?> :
+            </div>
+            <div class="field-items">
+                <div class="field-item even">
+                    <?php print mica_commons_get_localized_field($context_detail['data']->methods, 'followUpInfo'); ?>
+                </div>
+            </div>
+        </div>
+        <?php endif; ?>
+
+
+        <?php if(!empty($context_detail['data']->methods->recruitments)): ?>
+        <div class="field field-name-field-recruitment field-type-list-text field-label-inline clearfix">
+            <div class="field-label">
+                <?php print t('Recruitment target') ?> :
+            </div>
+            <div class="field-items">
+                <?php foreach ($context_detail['data']->methods->recruitments as $recruitment): ?>
+                    <div class="field-item">
+                        <?php if ($recruitment == 'other'): ?>
+                            <div class="field-label">
+                                <?php print $recruitment ?> :
+                            </div>
+                            <span class="other-sub-value">
+                                     <?php print mica_commons_get_localized_field($context_detail['data']->methods, 'otherRecruitment'); ?>
+                            </span>
+                        <?php else: ?>
+                            <?php print $recruitment; ?>
+                        <?php endif; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <?php endif; ?>
+
+        <?php if(!empty($context_detail['data']->numberOfParticipants->participant->number)): ?>
+        <div class="field field-name-field-target-number-participants field-type-text field-label-inline clearfix">
+            <div class="field-label">
+                <?php print t('Target number of participants') ?> :
+            </div>
+            <div class="field-items">
+                <div class="field-item even">
+                    <?php print $context_detail['data']->numberOfParticipants->participant->number; ?>
+                </div>
+                <div class="field-item odd">
+                    <?php print get_if_no_limite($context_detail['data']->numberOfParticipants->participant->noLimit); ?>
+                </div>
+            </div>
+        </div>
+        <?php endif; ?>
+
+        <?php if(!empty($context_detail['data']->numberOfParticipants->sample->number)): ?>
+        <div class="field field-name-field-target-number-biosamples field-type-text field-label-inline clearfix">
+            <div class="field-label">
+                <?php print t('Target number of participants with biological samples') ?> :
+            </div>
+            <div class="field-items">
+                <div class="field-item even">
+                    <?php print $context_detail['data']->numberOfParticipants->sample->number; ?>
+                </div>
+                <div class="field-item odd">
+                    <?php print get_if_no_limite($context_detail['data']->numberOfParticipants->sample->noLimit); ?>
+                </div>
+            </div>
+        </div>
+        <?php endif; ?>
+
+
+
+    </div>
+
+
 </section>
