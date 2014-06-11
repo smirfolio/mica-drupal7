fecbba7b968db7152e7836c3a13c64f90af8d9c3
samir <ben.sghaier.samir@gmail.com>
2014-06-11 10:58:36 -0400

    Pull request #4

--- drupal/modules/mica_client/mica_study/js/example.js
+++ drupal/modules/mica_client/mica_study/js/example.js
@@ -1,7 +1,7 @@
 (function ($) {
     Drupal.behaviors.datatables_add_head = {
         attach: function (context, settings) {
-            if (Drupal.settings.mica_study.study_json){
+            if (Drupal.settings.mica_study.study_json) {
                 new $.MicaTimeline(new $.StudyDtoParser()).create("#vis", JSON.parse(Drupal.settings.mica_study.study_json)).addLegend();
             }
         }
