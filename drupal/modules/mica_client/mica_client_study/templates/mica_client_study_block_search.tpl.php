a17085dce7000e6fbd9dd231fdbd2668b28d42bf
smirfolio <ben.sghaier.samir@gmail.com>
2014-07-10 17:00:14 -0400

    Debug facet's add more aggregations

--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study_block_search.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study_block_search.tpl.php
@@ -8,8 +8,8 @@
 <!--</ul>-->
 
 <?php //dpm($items['VAR_ONE']);?>
-<form id="search_<?php print $formId; ?>" method="GET">
-  <?php foreach ($items as $term => $term_count): ?>
+<form id="facet-search_<?php print $formId; ?>" method="GET">
+<?php foreach ($items as $term => $term_count): ?>
     <label class="checkbox">
       <?php print render($term_count); ?>
     </label>
