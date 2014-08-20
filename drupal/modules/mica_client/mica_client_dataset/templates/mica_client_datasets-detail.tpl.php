ca6641a339170f9927110ec9b1b6d148064352a6
Yannick Marcon <yannick.marcon@obiba.org>
2014-08-20 16:19:13 -0400

    MICASERVER-74 Dataset page reviewed (in progress)

--- drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-detail.tpl.php
+++ drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-detail.tpl.php
@@ -2,24 +2,21 @@
 //dpm($dataset_dto);
 //dpm($datset_type);
 ?>
-<?php if (!empty($dataset_dto->description)) : ?>
-  <article>
-    <header>
-    </header>
 
-    <div class="field field-name-body">
-      <div class="field-items">
-        <div class="field-label">
-          <?php print t('Description') ?> :
-        </div>
-        <div class="field-item even" property="content:encoded">
-          <p>
-            <?php print mica_client_commons_get_localized_field($dataset_dto, 'description'); ?>
-          </p>
-        </div>
-      </div>
-    </div>
-    <footer>
-    </footer>
-  </article>
+  <h3><?php print t('General Information') ?></h3>
+
+  <h5><?php print t('Dataset Type'); ?></h5>
+  <p>
+    <?php
+    if (!empty($datset_type->project)):
+      echo t('Harmonization dataset');
+    else:
+      echo t('Study dataset');
+    endif;
+    ?>
+  </p>
+
+<?php if (!empty($dataset_dto->description)): ?>
+  <h5><?php print t('Description'); ?></h5>
+  <p><?php print mica_client_commons_get_localized_field($dataset_dto, 'description'); ?></p>
 <?php endif; ?>
--- drupal/modules/mica_client/mica_client_dataset/templates/mica_client_variable-detail.tpl.php
+++ drupal/modules/mica_client/mica_client_dataset/templates/mica_client_variable-detail.tpl.php
@@ -2,27 +2,27 @@
 //dpm($variable_dto);
 ?>
 
-<h2><?php print t('General Information') ?></h2>
+<h3><?php print t('General Information') ?></h3>
 
 <?php if (!empty($variable_dto->label)): ?>
-  <h4><?php print t('Label') ?></h4>
+  <h5><?php print t('Label') ?></h5>
   <p><?php print $variable_dto->label; ?></p>
 <?php endif; ?>
 
 <?php if (!empty($variable_dto->description)): ?>
-  <h4><?php print t('Description'); ?></h4>
+  <h5><?php print t('Description'); ?></h5>
   <p><?php print $variable_dto->description; ?></p>
 <?php endif; ?>
 
 <?php if (!empty($variable_dto->studySummaries)): ?>
-  <h4>
+  <h5>
     <?php if ($variable_dto->variableType == 'Dataschema') {
       print t('Studies');
     }
     else {
       print t('Study');
     }?>
-  </h4>
+  </h5>
   <p>
     <?php
     foreach ($variable_dto->studySummaries as $studySummary) {
@@ -34,7 +34,7 @@
 <?php endif; ?>
 
 <?php if (!empty($variable_dto->datasetId)): ?>
-  <h4><?php print t('Dataset'); ?></h4>
+  <h5><?php print t('Dataset'); ?></h5>
   <p>
     <?php
     print l(mica_client_commons_get_localized_field($variable_dto, 'datasetName'), 'mica/dataset/' . mica_client_commons_to_slug(mica_client_commons_get_localized_field($variable_dto, 'datasetName')) .
@@ -43,30 +43,30 @@
   </p>
 <?php endif; ?>
 
-<h4><?php print t('Value Type'); ?></h4>
+<h5><?php print t('Value Type'); ?></h5>
 <p><?php print t($variable_dto->valueType); ?></p>
 
-<h4><?php print t('Variable Type'); ?></h4>
-<?php print t($variable_dto->variableType); ?>
+<h5><?php print t('Variable Type'); ?></h5>
+<p><?php print t($variable_dto->variableType); ?></p>
 
 <?php if (!empty($variable_dto->comment)): ?>
-  <h4><?php print t('Comment'); ?></h4>
-  <?php print($variable_dto->comment); ?>
+  <h5><?php print t('Comment'); ?></h5>
+  <p><?php print($variable_dto->comment); ?></p>
 <?php endif; ?>
 
 <?php if (!empty($variable_dto->categoriestab)): ?>
-  <h2><?php print t('Categories') ?></h2>
-  <?php print($variable_dto->categoriestab); ?>
+  <h3><?php print t('Categories') ?></h3>
+  <p><?php print($variable_dto->categoriestab); ?></p>
 <?php endif; ?>
 
-<h2><?php print t('Statistics') ?></h2>
+<h3><?php print t('Statistics') ?></h3>
 
 <div id="param-statistics" var-id="<?php print $variable_dto->id; ?>">
   <div id="txtblnk"> <?php print t('Please wait loading Statistics table....'); ?> </div>
 </div>
 
 <?php if (!empty($variable_dto->harmonizationtab)): ?>
-  <h2><?php print t('Harmonization') ?></h2>
+  <h3><?php print t('Harmonization') ?></h3>
   <?php print($variable_dto->harmonizationtab); ?>
 <?php endif; ?>
 
