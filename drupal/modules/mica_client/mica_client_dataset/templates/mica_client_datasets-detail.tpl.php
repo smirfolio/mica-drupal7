8527d4f27d22279726b47349659ff71664d87914
Yannick Marcon <yannick.marcon@obiba.org>
2014-08-21 10:50:13 -0400

    MICASERVER-74 Dataset page reviewed

--- drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-detail.tpl.php
+++ drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-detail.tpl.php
@@ -1,22 +1,22 @@
 <?php
 //dpm($dataset_dto);
-//dpm($datset_type);
+//dpm($dataset_type);
 ?>
 
-  <h3><?php print t('General Information') ?></h3>
-
-  <h5><?php print t('Dataset Type'); ?></h5>
-  <p>
-    <?php
-    if (!empty($datset_type->project)):
-      echo t('Harmonization dataset');
-    else:
-      echo t('Study dataset');
-    endif;
-    ?>
-  </p>
+<h3><?php print t('General Information') ?></h3>
 
 <?php if (!empty($dataset_dto->description)): ?>
   <h5><?php print t('Description'); ?></h5>
   <p><?php print mica_client_commons_get_localized_field($dataset_dto, 'description'); ?></p>
 <?php endif; ?>
+
+<h5><?php print t('Dataset Type'); ?></h5>
+<p>
+  <?php
+  if (!empty($dataset_type->project)):
+    echo t('Harmonization dataset');
+  else:
+    echo t('Study dataset');
+  endif;
+  ?>
+</p>
--- drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-tables.tpl.php
+++ drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-tables.tpl.php
@@ -4,13 +4,8 @@
 
 ?>
 <?php if (!empty($tab_var)): ?>
-  <?php if (!is_array($tab_var)): ?>
-    <h3><?php print $title_section ?></h3>
-    <?php print $tab_var; ?>
-  <?php elseif (is_array($tab_var) && !empty($tab_var['study-dataset'])): ?>
-    <h5><?php print t('Study') ?></h5>
-    <p><?php print l($tab_var['study-dataset']->studyId, 'mica/study/' . $tab_var['study-dataset']->studyId . '/' . $tab_var['study-dataset']->studyId); ?></p>
-  <?php endif; ?>
+  <h3><?php print $title_section ?></h3>
+  <?php print $tab_var; ?>
 <?php endif; ?>
 
 
