e70eba8df7dd16dac9f81600aa4a3f4135706e71
Yannick Marcon <yannick.marcon@obiba.org>
2014-08-28 16:29:08 -0400

    Code and style cleaning

--- drupal/modules/mica_client/mica_client_facet_search/templates/block--mica_client_facet_search.tpl.php
+++ drupal/modules/mica_client/mica_client_facet_search/templates/block--mica_client_facet_search.tpl.php
@@ -1,4 +1,4 @@
-<section id="<?php print $block_html_id; ?>" class="bootCollaplse <?php print $classes; ?>"<?php print $attributes; ?>>
+<section id="<?php print $block_html_id; ?>" class="boot-collapse <?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
