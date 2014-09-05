831a4608f8d66cc74affab3daba64d5ddeaa625d
smirfolio <ben.sghaier.samir@gmail.com>
2014-09-05 11:55:51 -0400

    Enhance Ui Changing responsive reaction

--- drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_charts.tpl.php
+++ drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_charts.tpl.php
@@ -1,32 +1,30 @@
 <?php //dpm($charts);?>
 
 <div class="panel panel-default">
-  <div class="container-fluid">
     <div class="row">
       <?php $nch = 0; ?>
       <?php foreach ($charts as $chart) : ?>
         <?php if ($nch < 4) : ?>
-          <div class="col-xs-3"><?php print render($chart); ?> </div>
+          <div class="col-md-3"><?php print render($chart); ?> </div>
         <?php endif; ?>
         <?php $nch++; ?>
       <?php endforeach; ?>
+
     </div>
-  </div>
 
   <div id="collapseOne" class="charts panel-collapse collapse">
     <div class="panel-body">
-      <div class="container-fluid">
         <div class="row">
         <?php $nch = 0; ?>
         <?php foreach ($charts as $chart) : ?>
           <?php if ($nch > 4) : ?>
-            <div class="col-xs-3"><?php print render($chart); ?> </div>
+            <div class="col-md-3"><?php print render($chart); ?> </div>
           <?php endif; ?>
           <?php $nch++; ?>
         <?php endforeach; ?>
         </div>
       </div>
-    </div>
+
   </div>
   <div class="botton-container">
     <span class="panel-title show-button">
