af73e9e4230dcb4cb4067b135373985de3a25f69
smirfolio <ben.sghaier.samir@gmail.com>
2014-09-05 10:32:30 -0400

    Enhance Ui add show more/less button

--- drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_charts.tpl.php
+++ drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_charts.tpl.php
@@ -1,21 +1,33 @@
 <?php //dpm($charts);?>
 
 <div class="panel panel-default">
-  <div class="panel-heading">
-    <h4 class="panel-title">
-      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-        <?php print t('Show all charts') ?>
-      </a>
-    </h4>
-  </div>
+  <?php $nch = 0; ?>
+  <?php foreach ($charts as $chart) : ?>
+    <?php if ($nch < 3) : ?>
+      <div class="col-md-4"><?php print render($chart); ?> </div>
+    <?php endif; ?>
+    <?php $nch++; ?>
+  <?php endforeach; ?>
+
+
   <div id="collapseOne" class="charts panel-collapse collapse">
     <div class="panel-body">
       <div class="row">
+        <?php $nch = 0; ?>
         <?php foreach ($charts as $chart) : ?>
-          <div class="col-md-4"><?php print render($chart); ?> </div>
+          <?php if ($nch > 3) : ?>
+            <div class="col-md-4"><?php print render($chart); ?> </div>
+          <?php endif; ?>
+          <?php $nch++; ?>
         <?php endforeach; ?>
       </div>
     </div>
   </div>
-
+  <div class="botton-container">
+    <span class="panel-title show-button">
+    <a class="text-button-field" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+      <?php print t('Show all') ?>
+    </a>
+      </span>
+  </div>
 </div>
