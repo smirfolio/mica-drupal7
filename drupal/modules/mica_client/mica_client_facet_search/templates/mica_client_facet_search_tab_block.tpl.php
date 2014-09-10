ae2ffe6fc8aa0ea473306bb7a44d3ab24883d88d
smirfolio <ben.sghaier.samir@gmail.com>
2014-09-10 14:35:33 -0400

    Debug search page probleme on switching tabs

--- drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_tab_block.tpl.php
+++ drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_tab_block.tpl.php
@@ -1,15 +1,16 @@
 <?php //dvm('search var',$studies);
 ?>
+<div id="search-facets">
+  <ul class="nav nav-tabs" role="tablist">
+    <li class="active"><a href="#variable-facet" role="tab" data-toggle="tab"><?php print t('Variables') ?> </a></li>
+    <li><a href="#study-facet" role="tab" data-toggle="tab"> <?php print t('Studies') ?> </a></li>
 
-<ul class="nav nav-tabs" role="tablist">
-  <li class="active"><a href="#variable-facet" role="tab" data-toggle="tab"><?php print t('Variables') ?> </a></li>
-  <li><a href="#study-facet" role="tab" data-toggle="tab"> <?php print t('Studies') ?> </a></li>
+  </ul>
 
-</ul>
+  <!-- Tab panes -->
+  <div class="tab-content search-facets">
+    <div class="tab-pane active" id="variable-facet"> <?php print render($mica_client_dataset); ?></div>
+    <div class="tab-pane" id="study-facet"> <?php print  render($mica_client_study); ?></div>
 
-<!-- Tab panes -->
-<div class="tab-content">
-  <div class="tab-pane active" id="variable-facet"> <?php print render($mica_client_dataset); ?></div>
-  <div class="tab-pane" id="study-facet"> <?php print  render($mica_client_study); ?></div>
-
+  </div>
 </div>
