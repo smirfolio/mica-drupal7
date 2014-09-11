09ca1859be0405645474d47078f2f071886677cc
smirfolio <ben.sghaier.samir@gmail.com>
2014-09-11 18:05:43 -0400

    Save tab facetes stat in cookies ...in progress

--- drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-detail.tpl.php
+++ drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-detail.tpl.php
@@ -62,7 +62,7 @@
           print l(t('Search Variables'), 'mica/search',
             array(
               'query' => array(
-                'tab' => 'variables',
+                'type' => 'variables',
                 'child:datasetId[]' => 'datasetId.' . $dataset_dto->id
               ),
               'attributes' => array('class' => 'btn btn-primary')
