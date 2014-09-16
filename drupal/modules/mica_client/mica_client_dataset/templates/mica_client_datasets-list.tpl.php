8d22bb014b9b4ac86b62b4b3084aa05dcc10a330
smirfolio <ben.sghaier.samir@gmail.com>
2014-09-16 14:46:26 -0400

    Add list Datasets page

--- drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-list.tpl.php
+++ drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-list.tpl.php
@@ -1,4 +1,39 @@
-<?php //dpm($context_datasets_list);?>
-<?php if (!empty($context_datasets_list)): ?>
-  <?php print $context_datasets_list['dataset-tab']; ?>
-<?php endif; ?>
+<?php dpm($datasets); ?>
+<?php print render($node_page) ?>
+<hr>
+<div class="clearfix">
+  <div class="alert alert-info col-md-7" role="alert"><strong>TODO</strong>search form here
+  </div>
+  <div class="col-md-5 ">
+    <div class="studies-link-center">
+      <?php print t('Open IN'); ?> : <a href="search?type=studies"><?php print t('Advanced search'); ?></a>
+      <!--      |      <a href="">--><?php //print t('Domain Coverage'); ?><!-- </a>-->
+    </div>
+  </div>
+  <div class="clearfix"></div>
+
+  <div class="list-page">
+
+    <?php foreach ($datasets as $dataset) :
+      $dataset_name = mica_client_commons_get_localized_field($dataset, 'name');
+      ?>
+
+      <div class="study-item">
+        <h1 class="big-caracter">
+           <span class="t_badge color_D">
+          D
+<!--             --><?php //print drupal_substr(mica_client_commons_get_localized_field($dataset, 'name'), 0, 1); ?>
+           </span>
+          <a href="../<?php print $type ?>/<?php print $dataset->id ?>/<?php print $dataset_name ?>">
+            <?php print  $dataset_name; ?>
+          </a>
+        </h1>
+        <?php if (!empty($dataset)): ?>
+          <p>
+            <?php print   mica_client_commons_get_localized_field($dataset, 'description'); ?>
+          </p>
+        <?php endif; ?>
+      </div>
+      <div class="clearfix"></div>
+    <?php endforeach; ?>
+  </div>
