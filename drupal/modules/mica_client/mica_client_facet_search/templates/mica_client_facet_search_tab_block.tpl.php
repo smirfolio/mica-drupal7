361ad75a26b45e3b64a1e2cd677a1678a917f8c7
smirfolio <ben.sghaier.samir@gmail.com>
2014-09-18 12:13:36 -0400

    Facet block construction

--- drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_tab_block.tpl.php
+++ drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_tab_block.tpl.php
@@ -2,14 +2,14 @@
 ?>
 <div id="search-facets">
   <ul class="nav nav-tabs facets-tab" role="tablist" id="facet-search">
-  <li class="active"><a href="#variable-facet" role="tab" data-toggle="tab"><?php print t('Variables') ?> </a></li>
+    <li class="active"><a href="#variable-facet" role="tab" data-toggle="tab"><?php print t('Variables') ?> </a></li>
     <li><a href="#study-facet" role="tab" data-toggle="tab"> <?php print t('Studies') ?> </a></li>
 
   </ul>
 
   <!-- Tab panes -->
   <div class="tab-content search-facets">
-    <div class="tab-pane active" id="variable-facet"> <?php print render($mica_client_dataset); ?></div>
+    <div class="tab-pane active" id="variable-facet"> <?php print render($mica_client_variable); ?></div>
     <div class="tab-pane" id="study-facet"> <?php print  render($mica_client_study); ?></div>
 
   </div>
