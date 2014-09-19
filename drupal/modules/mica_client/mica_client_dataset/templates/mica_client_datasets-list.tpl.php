43eefbf245798af031686bdf8999f64f744ed460
Yannick Marcon <yannick.marcon@obiba.org>
2014-09-19 12:45:47 -0400

    MICASERVER-89 Study list page layout reviewed

--- drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-list.tpl.php
+++ drupal/modules/mica_client/mica_client_dataset/templates/mica_client_datasets-list.tpl.php
@@ -6,31 +6,26 @@
   </div>
   <div class="col-md-5 ">
     <div class="advanced-search-link-center">
-    <?php print t('Open IN'); ?> : <a href="search?type=studies"><?php print t('Advanced search'); ?></a>
+      <?php print t('Open IN'); ?> : <a href="search?type=studies"><?php print t('Advanced search'); ?></a>
       <!--      |      <a href="">--><?php //print t('Domain Coverage'); ?><!-- </a>-->
     </div>
   </div>
   <div class="clearfix"></div>
 
   <div class="list-page">
-
     <?php foreach ($datasets as $dataset) :
       $dataset_name = mica_client_commons_get_localized_field($dataset, 'name');
       ?>
-
-
-      <h1 class="big-caracter">
-           <span class="t_badge color_D">
-          D
-<!--             --><?php //print drupal_substr(mica_client_commons_get_localized_field($dataset, 'name'), 0, 1); ?>
-           </span>
+      <h4>
+        <?php //print drupal_substr(mica_client_commons_get_localized_field($dataset, 'name'), 0, 1); ?>
+        </span>
         <a href="../<?php print $type ?>/<?php print $dataset->id ?>/<?php print $dataset_name ?>">
-          <?php print  $dataset_name; ?>
+          <?php print mica_client_commons_get_localized_field($dataset, 'acronym') . ' - ' . $dataset_name; ?>
         </a>
-      </h1>
+      </h4>
       <?php if (!empty($dataset)): ?>
       <p>
-        <?php print   mica_client_commons_get_localized_field($dataset, 'description'); ?>
+        <?php print truncate_utf8(mica_client_commons_get_localized_field($dataset, 'description'), 300, TRUE, TRUE); ?>
       </p>
     <?php endif; ?>
 
