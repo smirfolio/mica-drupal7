4e21fd86e177a00fc33fbc1fb0c9233f16bf88ee
smirfolio <ben.sghaier.samir@gmail.com>
2014-09-20 01:00:02 -0400

    Page search study list

--- drupal/modules/mica_client/mica_client_study/js/ajax_getlist_items.js
+++ drupal/modules/mica_client/mica_client_study/js/ajax_getlist_items.js
@@ -4,7 +4,7 @@
       /*******************/
       var text_studies = Drupal.t('Studies');
       var text_study = Drupal.t('Study');
-      $("#edit-studies-query").on("keypress", function () {
+      $("#edit-studies-query").on("keypress blur", function () {
         $.ajax({
           url: 'refresh_list_studies/' + $(this).val() + '/' + $("#edit-studies-sort").val() + '/' + $("#edit-studies-sort-order").val(),
           success: function (data) {
@@ -17,18 +17,11 @@
         });
       });
 
-      $("#edit-studies-query").on("blur", function () {
-        $.ajax({
-          url: 'refresh_list_studies/' + $(this).val() + '/' + $("#edit-studies-sort").val() + '/' + $("#edit-studies-sort-order").val(),
-          success: function (data) {
-            if (data) {
-              $('#refresh-list').empty().append(data.list_studies);
-              var testStudy = (data.total > 1) ? text_studies : text_study;
-              $('#refrech-count').empty().append(data.total + ' ' + testStudy);
-            }
-          }
+        $("#edit-studies-query").on("blur", function () {
+            var data_url= $('#mica-client-study-create-form').serialize();
+            window.location = '?' +data_url;
         });
-      });
+
 
       $("#edit-studies-sort-order").on("change", function () {
 
