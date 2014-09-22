9c16d781b8bfdd1a1d7dba7a2352de0fc93c6fa7
smirfolio <ben.sghaier.samir@gmail.com>
2014-09-22 12:03:39 -0400

    debug Search page  add icon badge

--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study-list-page-block.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study-list-page-block.tpl.php
@@ -11,7 +11,7 @@
                width="120" height="96" alt="">
         <?php else : ?>
           <h1 class="big-character">
-            <span class="t_badge color_S"><?php print t('S') ?></span>
+            <span class="t_badge color_S"></span>
           </h1>
         <?php endif; ?>
       </div>
