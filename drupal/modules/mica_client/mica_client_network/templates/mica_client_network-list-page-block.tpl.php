7f19f918d110f4f3cad64fe766e2089abb667f2b
Ramin Haeri Azad <rhaeri@p3g.org>
2014-09-29 16:28:52 -0400

    feature-MICASERVER-87 - implemented the network page and refractored the common search functionality and helper JS classes

--- drupal/modules/mica_client/mica_client_network/templates/mica_client_network-list-page-block.tpl.php
+++ drupal/modules/mica_client/mica_client_network/templates/mica_client_network-list-page-block.tpl.php
@@ -1,5 +1,5 @@
 <?php if (!empty($list_networks) && !empty($list_networks->networks)): ?>
-  
+
   <?php foreach ($list_networks->networks as $network) :
     $network_name = mica_client_commons_get_localized_field($network, 'name');
     ?>
@@ -9,13 +9,6 @@
         print l(mica_client_commons_get_localized_field($network, 'acronym') . ' - ' . $network_name,
           'mica/network/' . $network->id); ?>
       </h4>
-<!--      --><?php //if (!empty($network->description)): ?>
-<!--        <p>-->
-<!--          --><?php //print truncate_utf8(mica_client_commons_get_localized_field($network, 'description'), 300, TRUE, TRUE); ?>
-<!--        </p>-->
-<!--      --><?php //else: ?>
-<!--        <i><small>--><?php //print t('No description'); ?><!--</small></i>-->
-<!--      --><?php //endif; ?>
     </div>
   <?php endforeach; ?>
 <?php endif; ?>
