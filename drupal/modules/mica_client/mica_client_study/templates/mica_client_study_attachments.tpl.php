42c1746cee28a77ae985102a2b96159d3ab72ad4
smirfolio <ben.sghaier.samir@gmail.com>
2014-10-01 09:39:12 -0400

    Bug link to query Variable search in Datasets details page Download with the correct name attachment files

--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study_attachments.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study_attachments.tpl.php
@@ -2,9 +2,10 @@
         <a
           href="<?php print mica_client_commons_safe_expose_server_url($study_id, $attachment, 'study') ?>"
           type="<?php print $attachment->type; ?>; length=<?php print $attachment->size; ?>"
-          title="<?php print $attachment->fileName; ?>">
-          <span class="glyphicon glyphicon-download"></span>
+          title="<?php print $attachment->fileName; ?>"
+          download="<?php print rawurldecode($attachment->fileName); ?>"
+          >
+        <span class="glyphicon glyphicon-download"></span>
           <?php print urldecode($attachment->fileName); ?></a>
-
 </li>
 
