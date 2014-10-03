9ca7e4e00791706b593f92fe71ee6604687e275c
Ramin Haeri Azad <rhaeri@p3g.org>
2014-10-03 14:22:11 -0400

    new-panel-document-item-list

--- drupal/modules/mica_client/mica_client_network/templates/mica_client_network-list-page-block.tpl.php
+++ drupal/modules/mica_client/mica_client_network/templates/mica_client_network-list-page-block.tpl.php
@@ -1,5 +1,4 @@
 <?php if (!empty($network) && !empty($network)): ?>
-
   <div class="row lg-bottom-margin">
     <div class="col-md-2 col-xs-2 text-center">
       <?php if (!empty($logo_url)): ?>
@@ -12,15 +11,29 @@
       <?php endif; ?>
     </div>
     <div class="col-md-10 col-xs-10">
-      <h4><a
-          href="network/<?php print $network->id ?>">
-          <?php print mica_client_commons_get_localized_field($network, 'acronym') . ' - ' . mica_client_commons_get_localized_field($network, 'name'); ?>
-        </a>
-      </h4>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <a
+            href="network/<?php print $network->id ?>">
+            <?php print mica_client_commons_get_localized_field($network, 'acronym') . ' - ' . mica_client_commons_get_localized_field($network, 'name'); ?>
+          </a>
+        </div>
+        <div class="panel-body">
+          <p>
 
-      <p>
-        <?php // print truncate_utf8(mica_client_commons_get_localized_field($network, 'objectives'), 300, TRUE, TRUE); ?>
-      </p>
+            <?php print empty($network->description) ? '' : truncate_utf8(mica_client_commons_get_localized_field($network, 'description'), 300, TRUE, TRUE);; ?>
+          </p>
+
+          <p class="lg-top-margin">
+            <?php
+            $count = empty($network->studyIds) ? 0 : count($network->studyIds);
+            $caption = $count < 2 ? "study" : "studies";
+            $href = "<a href='studies'>$count $caption</a>";
+            ?>
+            <?php print t("Includes $href") ?>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 
--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study-list-page-block.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study-list-page-block.tpl.php
@@ -13,14 +13,19 @@
     <?php endif; ?>
   </div>
   <div class="col-md-10 col-xs-10">
-    <h4><a
-        href="study/<?php print $study->id ?>">
-        <?php print mica_client_commons_get_localized_field($study, 'acronym') . ' - ' . mica_client_commons_get_localized_field($study, 'name'); ?>
-      </a>
-    </h4>
-
-    <p>
-      <?php print truncate_utf8(mica_client_commons_get_localized_field($study, 'objectives'), 300, TRUE, TRUE); ?>
-    </p>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <a
+          href="study/<?php print $study->id ?>">
+          <?php print mica_client_commons_get_localized_field($study, 'acronym') . ' - ' . mica_client_commons_get_localized_field($study, 'name'); ?>
+        </a>
+      </div>
+      <div class="panel-body">
+        <p class="lg-top-margin">
+          <?php print truncate_utf8(mica_client_commons_get_localized_field($study, 'objectives'), 300, TRUE, TRUE); ?>
+        </p>
+      </div>
+    </div>
   </div>
 </div>
+
