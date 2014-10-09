04f64cb3998a135241815122cfd4029013c4fc79
Ramin Haeri Azad <rhaeri@p3g.org>
2014-10-08 20:17:37 -0400

    changed study list style

--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study-list-page-block.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study-list-page-block.tpl.php
@@ -30,6 +30,18 @@
     </div>
     <ul class="obiba-fa-list lg-top-margin help-block">
       <li class="no-margin no-padding">
+        <?php foreach($network_digests as $digest) {
+          $names = array();
+          array_push($names, mica_client_commons_get_localized_field($digest, 'name'));
+        }
+
+        if (!empty($names)) {
+          print t('Member of ') . implode(', and', $names);
+        }
+        ?>
+      </li>
+
+      <li class="no-margin no-padding">
         <?php print empty($study->designs) ? '' : t('Study design')?>: <span><?php print implode(', ', $study->designs)?></span>
         <?php print empty($study->targetNumber) ? '' : t('; Target number of participants')?>: <span><?php print $study->targetNumber->noLimit
             ? t('No limits') : $study->targetNumber->number ?></span>
@@ -44,21 +56,8 @@
         ?>
         <span>
           <a class="badge" href=''><?php print $datasets ?></a> <?php print $dataset_caption ?>,
-          <a href=''><?php print $vars?></a> <?php print $dataset_caption?>
+          <a class="badge" href=''><?php print $vars?></a> <?php print $var_caption?>
         </span>
-      </li>
-      <li class="no-margin no-padding">
-        <?php foreach($network_digests as $digest) {
-          $names = array();
-          array_push($names, mica_client_commons_get_localized_field($digest, 'name'));
-        }
-
-        if (!empty($names)) {
-          print t('Member of ') . implode(', and', $names);
-        }
-
-        ?>
-
       </li>
 
     </div>
