b2ef05b18b2cfd5175021ab9d4b0c3c1a3be68e7
Yannick Marcon <yannick.marcon@obiba.org>
2014-10-14 11:24:55 -0400

    Case server is not responding when requestinmg coverage

--- drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_coverage.tpl.php
+++ drupal/modules/mica_client/mica_client_facet_search/templates/mica_client_facet_search_coverage.tpl.php
@@ -8,10 +8,17 @@
   <div id="search-query"></div>
   <div>
     <p class="pull-left">
-      <?php print t('%hits variables (among %count)', array(
-        '%hits' => $coverages->totalHits,
-        '%count' => $coverages->totalCount
-      )) ?>
+
+      <?php if (!empty($coverages->totalHits)): ?>
+        <?php print t('%hits variables (among %count)', array(
+          '%hits' => $coverages->totalHits,
+          '%count' => $coverages->totalCount
+        )) ?>
+      <?php else: ?>
+        <?php print t('%hits variables', array(
+          '%hits' => 0
+        )) ?>
+      <?php endif ?>
       <?php
       print l(t('Search'), 'mica/search', array(
         'attributes' => array(
@@ -67,46 +74,28 @@
 <?php $has_coverage = FALSE; ?>
 
   <article class="pull-left">
-    <?php foreach ($coverages->taxonomies as $taxonomy_coverage) : ?>
-      <?php if (!empty($taxonomy_coverage->hits) && !empty($taxonomy_coverage->vocabularies)): ?>
+    <?php if (!empty($coverages->taxonomies)): ?>
+      <?php foreach ($coverages->taxonomies as $taxonomy_coverage) : ?>
+        <?php if (!empty($taxonomy_coverage->hits) && !empty($taxonomy_coverage->vocabularies)): ?>
 
-        <section>
-<!--          --><?php //if (!empty($taxonomy_coverage->buckets)): ?>
-<!--            <div class="btn-group pull-right">-->
-<!--              <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">-->
-<!--                --><?php //print t('Display'); ?><!-- <span class="caret"></span>-->
-<!--              </button>-->
-<!--              <ul class="dropdown-menu" role="menu">-->
-<!--                <li><a href="#">--><?php //print t('All'); ?>
-<!--                    <div class="pull-right help-inline">--><?php //print $taxonomy_coverage->hits; ?><!--</div>-->
-<!--                  </a></li>-->
-<!--                <li class="divider"></li>-->
-<!--                --><?php //foreach ($taxonomy_coverage->buckets as $bucket): ?>
-<!--                  <li><a href="#--><?php //print $bucket->value; ?><!--">--><?php //print $bucket->value; ?>
-<!--                      <div class="pull-right help-inline">--><?php //print $bucket->hits; ?><!--</div>-->
-<!--                    </a>-->
-<!--                  </li>-->
-<!--                --><?php //endforeach ?>
-<!--              </ul>-->
-<!--            </div>-->
-<!--          --><?php //endif ?>
+          <section>
+            <h3>
+              <?php print mica_client_commons_get_localized_field($taxonomy_coverage->taxonomy, 'titles'); ?>
+            </h3>
 
-          <h3>
-            <?php print mica_client_commons_get_localized_field($taxonomy_coverage->taxonomy, 'titles'); ?>
-          </h3>
-
-          <p class="help-block">
-            <?php print mica_client_commons_get_localized_field($taxonomy_coverage->taxonomy, 'descriptions'); ?>
-          </p>
-          <?php foreach ($taxonomy_coverage->vocabularies as $vocabulary_coverage) : ?>
-            <?php if (!empty($vocabulary_coverage->hits)): ?>
-              <?php $has_coverage = TRUE; ?>
-            <?php endif ?>
-            <?php print render($vocabulary_coverage_outputs[$taxonomy_coverage->taxonomy->name][$vocabulary_coverage->vocabulary->name]); ?>
-          <?php endforeach; ?>
-        </section>
-      <?php endif ?>
-    <?php endforeach; ?>
+            <p class="help-block">
+              <?php print mica_client_commons_get_localized_field($taxonomy_coverage->taxonomy, 'descriptions'); ?>
+            </p>
+            <?php foreach ($taxonomy_coverage->vocabularies as $vocabulary_coverage) : ?>
+              <?php if (!empty($vocabulary_coverage->hits)): ?>
+                <?php $has_coverage = TRUE; ?>
+              <?php endif ?>
+              <?php print render($vocabulary_coverage_outputs[$taxonomy_coverage->taxonomy->name][$vocabulary_coverage->vocabulary->name]); ?>
+            <?php endforeach; ?>
+          </section>
+        <?php endif ?>
+      <?php endforeach; ?>
+    <?php endif ?>
   </article>
 
 <?php if (!$has_coverage): ?>
