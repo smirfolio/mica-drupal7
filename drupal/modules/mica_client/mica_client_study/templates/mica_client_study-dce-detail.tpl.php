d3a175321c8abbd685ad801ff4da1c6cb5786893
Yannick Marcon <yannick.marcon@obiba.org>
2014-10-16 09:28:04 -0400

    MICASERVER-129 Coverage link added to populations and DCEs

--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study-dce-detail.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study-dce-detail.tpl.php
@@ -1,3 +1,7 @@
+<?php
+//dpm($dce);
+?>
+
 <div id="<?php print $dce->id ?>" class="modal fade" xmlns="http://www.w3.org/1999/html">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -8,76 +12,131 @@
       <div class="modal-body">
 
         <section>
-          <?php if (!empty($dce->description)): ?>
-            <p>
-              <?php print mica_client_commons_get_localized_field($dce, 'description'); ?>
-            </p>
-          <?php endif; ?>
+          <div>
+            <?php if (!empty($dce->description)): ?>
+              <p>
+                <?php print mica_client_commons_get_localized_field($dce, 'description'); ?>
+              </p>
+            <?php endif; ?>
 
-          <?php if (!empty($dce->startYear)): ?>
-            <h5><?php print t('Start Year') ?></h5>
-            <p><?php print mica_client_commons_format_year($dce->startYear, $dce->startMonth); ?></p>
-          <?php endif; ?>
+            <div class="pull-right md-bottom-margin">
+              <?php
+              print l(t('Search Variables'), 'mica/search',
+                array(
+                  'query' => array(
+                    'type' => 'variables',
+                    'query' => '{"variables":{"terms":{"dceIds":["' . $dce_uid . '"]}}}'
+                  ),
+                  'attributes' => array('class' => 'btn btn-primary')
+                ));
+              ?>
+              <?php
+              print l(t('Coverage'), 'mica/coverage',
+                array(
+                  'query' => array(
+                    'type' => 'variables',
+                    'query' => '{"variables":{"terms":{"dceIds":["' . $dce_uid . '"]}}}'
+                  ),
+                  'attributes' => array('class' => 'btn btn-primary indent')
+                ));
+              ?>
+            </div>
+          </div>
 
-          <?php if (!empty($dce->endYear)): ?>
-            <h5><?php print t('End Year') ?></h5>
-            <p><?php print mica_client_commons_format_year($dce->endYear, $dce->endMonth); ?></p>
-          <?php endif; ?>
+          <div class="clearfix"></div>
 
-          <?php if (!empty($dce->dataSources)): ?>
-            <h5><?php print t('Data Sources') ?></h5>
-            <ul>
-              <?php foreach ($dce->dataSources as $dataSource): ?>
-                <li>
-                  <?php print t($dataSource); ?>
-                </li>
-              <?php endforeach; ?>
-              <?php if (!empty($dce->otherDataSources)): ?>
-                <li>
-                  <?php print mica_client_commons_get_localized_field($dce, 'otherDataSources'); ?>
-                </li>
-              <?php endif; ?>
-            </ul>
-          <?php endif; ?>
+          <table class="table table-striped">
+            <tbody>
+            <?php if (!empty($dce->startYear)): ?>
+              <tr>
+                <td><h5><?php print t('Start Year') ?></h5></td>
+                <td><p><?php print mica_client_commons_format_year($dce->startYear, $dce->startMonth); ?></p></td>
+              </tr>
+            <?php endif; ?>
 
-          <?php if (!empty($dce->administrativeDatabases)): ?>
-            <h5><?php print t('Administrative Databases') ?></h5>
-            <ul>
-              <?php foreach ($dce->administrativeDatabases as $database): ?>
-                <li>
-                  <?php print $database; ?>
-                </li>
-              <?php endforeach; ?>
-            </ul>
-          <?php endif; ?>
+            <?php if (!empty($dce->endYear)): ?>
+              <tr>
+                <td><h5><?php print t('End Year') ?></h5></td>
+                <td><p><?php print mica_client_commons_format_year($dce->endYear, $dce->endMonth); ?></p></td>
+              </tr>
+            <?php endif; ?>
 
-          <?php if (!empty($dce->bioSamples)): ?>
-            <h5><?php print t('Bio Samples') ?>:</h5>
-            <ul>
-              <?php foreach ($dce->bioSamples as $samples): ?>
-                <li>
-                  <?php print $samples; ?>
-                </li>
-              <?php endforeach; ?>
-              <?php if (!empty($dce->otherBioSamples)): ?>
-                <li>
-                  <?php print mica_client_commons_get_localized_field($dce, 'otherBioSamples'); ?>
-                </li>
-              <?php endif; ?>
-            </ul>
-          <?php endif; ?>
+            <?php if (!empty($dce->dataSources)): ?>
+              <tr>
+                <td><h5><?php print t('Data Sources') ?></h5></td>
+                <td>
+                  <ul>
+                    <?php foreach ($dce->dataSources as $dataSource): ?>
+                      <li>
+                        <?php print t($dataSource); ?>
+                      </li>
+                    <?php endforeach; ?>
+                    <?php if (!empty($dce->otherDataSources)): ?>
+                      <li>
+                        <?php print mica_client_commons_get_localized_field($dce, 'otherDataSources'); ?>
+                      </li>
+                    <?php endif; ?>
+                  </ul>
+                </td>
+              </tr>
+            <?php endif; ?>
 
-          <?php if (!empty($dce->tissueTypes)): ?>
-            <h5><?php print t('Tissue Types') ?></h5>
-            <p><?php print mica_client_commons_get_localized_field($dce, 'tissueTypes'); ?></p>
-          <?php endif; ?>
+            <?php if (!empty($dce->administrativeDatabases)): ?>
+              <tr>
+                <td><h5><?php print t('Administrative Databases') ?></h5></td>
+                <td>
+                  <ul>
+                    <?php foreach ($dce->administrativeDatabases as $database): ?>
+                      <li>
+                        <?php print $database; ?>
+                      </li>
+                    <?php endforeach; ?>
+                  </ul>
+                </td>
+              </tr>
+            <?php endif; ?>
 
-          <?php if (!empty($attachements)): ?>
-            <h5><?php print t('Documents') ?></h5>
-            <ul class="list-group">
-              <?php print $attachements; ?>
-            </ul>
-          <?php endif; ?>
+            <?php if (!empty($dce->bioSamples)): ?>
+              <tr>
+                <td><h5><?php print t('Bio Samples') ?>:</h5></td>
+                <td>
+                  <ul>
+                    <?php foreach ($dce->bioSamples as $samples): ?>
+                      <li>
+                        <?php print $samples; ?>
+                      </li>
+                    <?php endforeach; ?>
+                    <?php if (!empty($dce->otherBioSamples)): ?>
+                      <li>
+                        <?php print mica_client_commons_get_localized_field($dce, 'otherBioSamples'); ?>
+                      </li>
+                    <?php endif; ?>
+                  </ul>
+                </td>
+              </tr>
+            <?php endif; ?>
+
+            <?php if (!empty($dce->tissueTypes)): ?>
+              <tr>
+                <td><h5><?php print t('Tissue Types') ?></h5></td>
+                <td><p><?php print mica_client_commons_get_localized_field($dce, 'tissueTypes'); ?></p></td>
+              </tr>
+            <?php endif; ?>
+
+            <?php if (!empty($attachements)): ?>
+              <tr>
+                <td><h5><?php print t('Documents') ?></h5></td>
+                <td>
+                  <ul class="list-group">
+                    <?php print $attachements; ?>
+                  </ul>
+                </td>
+              </tr>
+            <?php endif; ?>
+
+            </tbody>
+          </table>
+
         </section>
       </div>
     </div>
--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study-population-detail.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study-population-detail.tpl.php
@@ -1,63 +1,94 @@
-<?php //dpm($context_population_detail); ?>
+<?php
+//dpm($population);
+?>
 
-  <p>
-    <?php print mica_client_commons_get_localized_field($context_population_detail, 'description'); ?>
-  </p>
+  <div>
+    <?php if (!empty($population->description)): ?>
+      <p class="pull-left">
+        <?php print mica_client_commons_get_localized_field($population, 'description'); ?>
+      </p>
+    <?php endif; ?>
+
+    <div class="pull-right md-bottom-margin">
+      <?php
+      print l(t('Search Variables'), 'mica/search',
+        array(
+          'query' => array(
+            'type' => 'variables',
+            'query' => '{"variables":{"terms":{"dceIds":["' . implode('","', $dce_uids) . '"]}}}'
+          ),
+          'attributes' => array('class' => 'btn btn-primary')
+        ));
+      ?>
+      <?php
+      print l(t('Coverage'), 'mica/coverage',
+        array(
+          'query' => array(
+            'type' => 'variables',
+            'query' => '{"variables":{"terms":{"dceIds":["' . implode('","', $dce_uids) . '"]}}}'
+          ),
+          'attributes' => array('class' => 'btn btn-primary indent')
+        ));
+      ?>
+    </div>
+  </div>
+
+  <div class="clearfix"></div>
 
   <h5><?php print t('Selection criteria') ?></h5>
   <ul>
-    <?php if (isset($context_population_detail->selectionCriteria->gender) && ($context_population_detail->selectionCriteria->gender === 0 || $context_population_detail->selectionCriteria->gender === 1)): ?>
+    <?php if (isset($population->selectionCriteria->gender) && ($population->selectionCriteria->gender === 0 || $population->selectionCriteria->gender === 1)): ?>
       <li>
         <i><?php print t('Gender') ?></i> :
-        <?php print  mica_client_study_get_gender($context_population_detail->selectionCriteria->gender); ?>
+        <?php print  mica_client_study_get_gender($population->selectionCriteria->gender); ?>
       </li>
     <?php endif; ?>
 
-    <?php if (!empty($context_population_detail->selectionCriteria->ageMin) || !empty($context_population_detail->selectionCriteria->ageMax)): ?>
+    <?php if (!empty($population->selectionCriteria->ageMin) || !empty($population->selectionCriteria->ageMax)): ?>
       <li>
         <i><?php print t('Age') ?></i> :
-        <?php !empty($context_population_detail->selectionCriteria->ageMin) ? print t('Minimum') . ' '
-          . $context_population_detail->selectionCriteria->ageMin : NULL; ?>,
-        <?php !empty($context_population_detail->selectionCriteria->ageMax) ? print t('Maximum') . ' '
-          . $context_population_detail->selectionCriteria->ageMax : NULL; ?>
+        <?php !empty($population->selectionCriteria->ageMin) ? print t('Minimum') . ' '
+          . $population->selectionCriteria->ageMin : NULL; ?>,
+        <?php !empty($population->selectionCriteria->ageMax) ? print t('Maximum') . ' '
+          . $population->selectionCriteria->ageMax : NULL; ?>
       </li>
     <?php endif; ?>
 
-    <?php if (!empty($context_population_detail->selectionCriteria->countriesIso)): ?>
+    <?php if (!empty($population->selectionCriteria->countriesIso)): ?>
       <li>
         <i><?php print t('Country'); ?></i> :
-        <?php mica_client_commons_iterate_field($context_population_detail->selectionCriteria->countriesIso,
+        <?php mica_client_commons_iterate_field($population->selectionCriteria->countriesIso,
           'countries_country_lookup', 'iso3', 'name'); ?>
       </li>
     <?php endif; ?>
 
-    <?php if (!empty($context_population_detail->selectionCriteria->ethnicOrigin)): ?>
+    <?php if (!empty($population->selectionCriteria->ethnicOrigin)): ?>
       <li>
         <i><?php print t('Ethnic origin') ?></i> :
-        <?php $ehtnic_origins = mica_client_commons_get_localized_dtos_field($context_population_detail->selectionCriteria, 'ethnicOrigin'); ?>
+        <?php $ehtnic_origins = mica_client_commons_get_localized_dtos_field($population->selectionCriteria, 'ethnicOrigin'); ?>
         <?php mica_client_commons_iterate_field($ehtnic_origins); ?>
       </li>
     <?php endif; ?>
 
-    <?php if (!empty($context_population_detail->selectionCriteria->healthStatus)): ?>
+    <?php if (!empty($population->selectionCriteria->healthStatus)): ?>
       <li>
         <i><?php print t('Health status') ?></i> :
-        <?php $health_status = mica_client_commons_get_localized_dtos_field($context_population_detail->selectionCriteria, 'healthStatus'); ?>
+        <?php $health_status = mica_client_commons_get_localized_dtos_field($population->selectionCriteria, 'healthStatus'); ?>
         <?php mica_client_commons_iterate_field($health_status); ?>
       </li>
     <?php endif; ?>
 
-    <?php if (!empty($context_population_detail->selectionCriteria->otherCriteria)): ?>
+    <?php if (!empty($population->selectionCriteria->otherCriteria)): ?>
       <li>
         <i><?php print t('Other') ?></i> :
-        <?php print mica_client_commons_get_localized_field($context_population_detail->selectionCriteria, 'otherCriteria'); ?>
+        <?php print mica_client_commons_get_localized_field($population->selectionCriteria, 'otherCriteria'); ?>
       </li>
     <?php endif; ?>
 
-    <?php if (!empty($context_population_detail->selectionCriteria->info)): ?>
+    <?php if (!empty($population->selectionCriteria->info)): ?>
       <li>
         <i><?php print t('Supplementary information') ?></i> :
-        <?php print mica_client_commons_get_localized_field($context_population_detail->selectionCriteria, 'info'); ?>
+        <?php print mica_client_commons_get_localized_field($population->selectionCriteria, 'info'); ?>
       </li>
     <?php endif; ?>
 
@@ -65,47 +96,47 @@
 
   <h5><?php print t('Sources of recruitment') ?></h5>
   <ul>
-    <?php if (!empty($context_population_detail->recruitment->generalPopulationSources)): ?>
+    <?php if (!empty($population->recruitment->generalPopulationSources)): ?>
       <li>
         <i><?php print t('General population') ?></i> :
-        <?php mica_client_commons_iterate_field($context_population_detail->recruitment->generalPopulationSources); ?>
+        <?php mica_client_commons_iterate_field($population->recruitment->generalPopulationSources); ?>
       </li>
     <?php endif; ?>
 
-    <?php if (!empty($context_population_detail->recruitment->studies)): ?>
+    <?php if (!empty($population->recruitment->studies)): ?>
       <li>
         <i><?php print t('Participants from existing studies') ?></i> :
         <?php
-        $studies = mica_client_commons_get_localized_dtos_field($context_population_detail->recruitment, 'studies');
+        $studies = mica_client_commons_get_localized_dtos_field($population->recruitment, 'studies');
         ?>
         <?php mica_client_commons_iterate_field($studies); ?>
       </li>
 
-      <?php if (!empty($context_population_detail->recruitment->specificPopulationSources)): ?>
+      <?php if (!empty($population->recruitment->specificPopulationSources)): ?>
         <li>
           <i><?php print t('Specific population') ?></i> :
-          <?php mica_client_commons_iterate_field($context_population_detail->recruitment->specificPopulationSources); ?>
+          <?php mica_client_commons_iterate_field($population->recruitment->specificPopulationSources); ?>
         </li>
       <?php endif; ?>
 
-      <?php if (!empty($context_population_detail->recruitment->otherSpecificPopulationSource)): ?>
+      <?php if (!empty($population->recruitment->otherSpecificPopulationSource)): ?>
         <li>
           <i><?php print t('Other specific population') ?></i> :
-          <?php print mica_client_commons_get_localized_field($context_population_detail->recruitment, 'otherSpecificPopulationSource'); ?>
+          <?php print mica_client_commons_get_localized_field($population->recruitment, 'otherSpecificPopulationSource'); ?>
         </li>
       <?php endif; ?>
 
-      <?php if (!empty($context_population_detail->recruitment->otherSource)): ?>
+      <?php if (!empty($population->recruitment->otherSource)): ?>
         <li>
           <i><?php print t('Supplementary information') ?></i> :
-          <?php print mica_client_commons_get_localized_field($context_population_detail->recruitment, 'otherSource'); ?>
+          <?php print mica_client_commons_get_localized_field($population->recruitment, 'otherSource'); ?>
         </li>
       <?php endif; ?>
 
-      <?php if (!empty($context_population_detail->recruitment->info)): ?>
+      <?php if (!empty($population->recruitment->info)): ?>
         <li>
           <?php print t('Supplementary information') ?> :
-          <p> <?php print mica_client_commons_get_localized_field($context_population_detail->recruitment, 'info'); ?></p>
+          <p> <?php print mica_client_commons_get_localized_field($population->recruitment, 'info'); ?></p>
         </li>
       <?php endif; ?>
 
@@ -113,41 +144,41 @@
 
   </ul>
 
-<?php if (!empty($context_population_detail->numberOfParticipants->participant->number)): ?>
+<?php if (!empty($population->numberOfParticipants->participant->number)): ?>
   <h5><?php print t('Number of participants') ?></h5>
   <p>
-    <?php print $context_population_detail->numberOfParticipants->participant->number . ' ' . t('participants') ?>
-    <?php if (!empty($context_population_detail->numberOfParticipants->participant->noLimit)): ?>
+    <?php print $population->numberOfParticipants->participant->number . ' ' . t('participants') ?>
+    <?php if (!empty($population->numberOfParticipants->participant->noLimit)): ?>
       (<?php print t('No limit'); ?>)
     <?php endif; ?>
   </p>
 <?php endif; ?>
 
-<?php if (!empty($context_population_detail->numberOfParticipants->sample->number)): ?>
+<?php if (!empty($population->numberOfParticipants->sample->number)): ?>
   <h5><?php print t('Number of participants') ?></h5>
   <p>
-    <?php print $context_population_detail->numberOfParticipants->sample->number . ' ' . t('participants') ?>
-    <?php if (!empty($context_population_detail->numberOfParticipants->sample->noLimit)): ?>
+    <?php print $population->numberOfParticipants->sample->number . ' ' . t('participants') ?>
+    <?php if (!empty($population->numberOfParticipants->sample->noLimit)): ?>
       (<?php print t('No limit'); ?>)
     <?php endif; ?>
   </p>
 <?php endif; ?>
 
-<?php if (!empty($context_population_detail->numberOfParticipants->info)): ?>
+<?php if (!empty($population->numberOfParticipants->info)): ?>
   <h5><?php print t('Supplementary information') ?></h5>
-  <p><?php print mica_client_commons_get_localized_field($context_population_detail->numberOfParticipants, 'info'); ?></p>
+  <p><?php print mica_client_commons_get_localized_field($population->numberOfParticipants, 'info'); ?></p>
 <?php endif; ?>
 
-<?php if (!empty($context_population_detail->info)): ?>
+<?php if (!empty($population->info)): ?>
   <h5><?php print t('Supplementary information') ?></h5>
-  <p> <?php print mica_client_commons_get_localized_field($context_population_detail, 'info'); ?></p>
+  <p> <?php print mica_client_commons_get_localized_field($population, 'info'); ?></p>
 <?php endif; ?>
 
-<?php if (!empty($context_population_detail['dce-tab'])): ?>
+<?php if (!empty($population['dce-tab'])): ?>
   <h5><?php print t('Data Collection Events') ?></h5>
-  <?php print $context_population_detail['dce-tab']; ?>
+  <?php print $population['dce-tab']; ?>
 <?php endif; ?>
 
-<?php if (!empty($context_population_detail['dce-modal'])): ?>
-  <div><?php print $context_population_detail['dce-modal']; ?></div>
+<?php if (!empty($population['dce-modal'])): ?>
+  <div><?php print $population['dce-modal']; ?></div>
 <?php endif; ?>
