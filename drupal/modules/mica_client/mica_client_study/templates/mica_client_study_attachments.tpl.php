3fece68b1a5091e55a47d14786a22ea25d2192d1
smirfolio <ben.sghaier.samir@gmail.com>
2015-01-08 11:51:35 -0500

    MK-207 Cache implementation for logo studies/networks download happens only on user click.

--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study_attachments.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study_attachments.tpl.php
@@ -1,11 +1,15 @@
 <li class="list-group-item">
-        <a
-          href="<?php print mica_client_commons_safe_expose_server_url($study_id, $attachment, 'study') ?>"
-          type="<?php print $attachment->type; ?>; length=<?php print $attachment->size; ?>"
-          title="<?php print $attachment->fileName; ?>"
-          download="<?php print rawurldecode($attachment->fileName); ?>"
-          >
-        <span class="glyphicon glyphicon-download"></span>
-          <?php print urldecode($attachment->fileName); ?></a>
+  <a
+    id="download-attachment"
+    entity="study"
+    id_entity="<?php print $study_id ?>"
+    id_attachment="<?php print $attachment->id ?>"
+    type="<?php print $attachment->type; ?>"
+    length="<?php print $attachment->size; ?>"
+    title="<?php print $attachment->fileName; ?>"
+    download="<?php print rawurldecode($attachment->fileName); ?>"
+    >
+    <span class="glyphicon glyphicon-download"></span>
+    <?php print urldecode($attachment->fileName); ?></a>
 </li>
 
