29e389e366181e06af7447598de3bda99ef90491
smirfolio <ben.sghaier.samir@gmail.com>
2015-01-08 15:33:28 -0500

    Debug tabs and modal navigation in study detail page sections alignment in study detail page

--- drupal/modules/mica_client/mica_client_study/templates/mica_client_study-population-detail.tpl.php
+++ drupal/modules/mica_client/mica_client_study/templates/mica_client_study-population-detail.tpl.php
@@ -49,12 +49,12 @@
 
 
   <div class="row ">
-    <div class="col-lg-6 col-xs-11 padding-left-15">
+    <div class="col-lg-6 col-xs-12 ">
       <h5><?php print t('Overview') ?></h5>
 
       <div class="scroll-content-tab">
         <table class="table table-striped">
-        <tbody>
+          <tbody>
 
           <?php if (!empty($population->numberOfParticipants->participant->number)): ?>
             <tr>
@@ -101,8 +101,8 @@
 
     </div>
 
-    <div class="col-lg-5 col-xs-11 padding-left-15">
-    <h5><?php print t('Sources of recruitment') ?></h5>
+    <div class="col-lg-6 col-xs-12 ">
+      <h5><?php print t('Sources of recruitment') ?></h5>
 
       <div class="scroll-content-tab">
         <table class="table table-striped">
@@ -160,77 +160,79 @@
   </div>
 
   <h5><?php print t('Selection criteria') ?></h5>
+  <div class="row ">
+    <div class="col-lg-12 col-xs-12">
+      <div class="scroll-content-tab">
+        <table class="table table-striped">
+          <tbody>
+          <?php if (isset($population->selectionCriteria->gender) && ($population->selectionCriteria->gender === 0 || $population->selectionCriteria->gender === 1)): ?>
+            <tr>
+              <td><h5><?php print t('Gender') ?></h5></td>
+              <td><?php print  mica_client_study_get_gender($population->selectionCriteria->gender); ?></td>
+            </tr>
+          <?php endif; ?>
 
-  <div class="scroll-content-tab">
-    <table class="table table-striped">
-      <tbody>
-      <?php if (isset($population->selectionCriteria->gender) && ($population->selectionCriteria->gender === 0 || $population->selectionCriteria->gender === 1)): ?>
-        <tr>
-          <td><h5><?php print t('Gender') ?></h5></td>
-          <td><?php print  mica_client_study_get_gender($population->selectionCriteria->gender); ?></td>
-        </tr>
-      <?php endif; ?>
+          <?php if (!empty($population->selectionCriteria->ageMin) || !empty($population->selectionCriteria->ageMax)): ?>
+            <tr>
+              <td><h5><?php print t('Age') ?></h5></td>
+              <td>
+                <?php !empty($population->selectionCriteria->ageMin) ? print t('Minimum') . ' '
+                  . $population->selectionCriteria->ageMin : NULL; ?>,
+                <?php !empty($population->selectionCriteria->ageMax) ? print t('Maximum') . ' '
+                  . $population->selectionCriteria->ageMax : NULL; ?>
+              </td>
+            </tr>
+          <?php endif; ?>
 
-      <?php if (!empty($population->selectionCriteria->ageMin) || !empty($population->selectionCriteria->ageMax)): ?>
-        <tr>
-          <td><h5><?php print t('Age') ?></h5></td>
-          <td>
-            <?php !empty($population->selectionCriteria->ageMin) ? print t('Minimum') . ' '
-              . $population->selectionCriteria->ageMin : NULL; ?>,
-            <?php !empty($population->selectionCriteria->ageMax) ? print t('Maximum') . ' '
-              . $population->selectionCriteria->ageMax : NULL; ?>
-          </td>
-        </tr>
-      <?php endif; ?>
+          <?php if (!empty($population->selectionCriteria->countriesIso)): ?>
+            <tr>
+              <td><h5><?php print t('Country'); ?></h5></td>
+              <td>
+                <?php mica_client_commons_iterate_field($population->selectionCriteria->countriesIso,
+                  'countries_country_lookup', 'iso3', 'name'); ?>
+              </td>
+            </tr>
+          <?php endif; ?>
 
-      <?php if (!empty($population->selectionCriteria->countriesIso)): ?>
-        <tr>
-          <td><h5><?php print t('Country'); ?></h5></td>
-          <td>
-            <?php mica_client_commons_iterate_field($population->selectionCriteria->countriesIso,
-              'countries_country_lookup', 'iso3', 'name'); ?>
-          </td>
-        </tr>
-      <?php endif; ?>
+          <?php if (!empty($population->selectionCriteria->ethnicOrigin)): ?>
+            <tr>
+              <td><h5><?php print t('Ethnic origin') ?></h5></td>
+              <td>
+                <?php $ehtnic_origins = mica_client_commons_get_localized_dtos_field($population->selectionCriteria, 'ethnicOrigin'); ?>
+                <?php mica_client_commons_iterate_field($ehtnic_origins); ?>
+              </td>
+            </tr>
+          <?php endif; ?>
 
-      <?php if (!empty($population->selectionCriteria->ethnicOrigin)): ?>
-        <tr>
-          <td><h5><?php print t('Ethnic origin') ?></h5></td>
-          <td>
-            <?php $ehtnic_origins = mica_client_commons_get_localized_dtos_field($population->selectionCriteria, 'ethnicOrigin'); ?>
-            <?php mica_client_commons_iterate_field($ehtnic_origins); ?>
-          </td>
-        </tr>
-      <?php endif; ?>
+          <?php if (!empty($population->selectionCriteria->healthStatus)): ?>
+            <tr>
+              <td><h5><?php print t('Health status') ?></h5></td>
+              <td>
+                <?php $health_status = mica_client_commons_get_localized_dtos_field($population->selectionCriteria, 'healthStatus'); ?>
+                <?php mica_client_commons_iterate_field($health_status); ?>
+              </td>
+            </tr>
+          <?php endif; ?>
 
-      <?php if (!empty($population->selectionCriteria->healthStatus)): ?>
-        <tr>
-          <td><h5><?php print t('Health status') ?></h5></td>
-          <td>
-            <?php $health_status = mica_client_commons_get_localized_dtos_field($population->selectionCriteria, 'healthStatus'); ?>
-            <?php mica_client_commons_iterate_field($health_status); ?>
-          </td>
-        </tr>
-      <?php endif; ?>
+          <?php if (!empty($population->selectionCriteria->otherCriteria)): ?>
+            <tr>
+              <td><h5><?php print t('Other') ?></h5></td>
+              <td><?php print mica_client_commons_get_localized_field($population->selectionCriteria, 'otherCriteria'); ?></td>
+            </tr>
+          <?php endif; ?>
 
-      <?php if (!empty($population->selectionCriteria->otherCriteria)): ?>
-        <tr>
-          <td><h5><?php print t('Other') ?></h5></td>
-          <td><?php print mica_client_commons_get_localized_field($population->selectionCriteria, 'otherCriteria'); ?></td>
-        </tr>
-      <?php endif; ?>
+          <?php if (!empty($population->selectionCriteria->info)): ?>
+            <tr>
+              <td><h5><?php print t('Supplementary information') ?></h5></td>
+              <td><?php print mica_client_commons_get_localized_field($population->selectionCriteria, 'info'); ?></td>
+            </tr>
+          <?php endif; ?>
 
-      <?php if (!empty($population->selectionCriteria->info)): ?>
-        <tr>
-          <td><h5><?php print t('Supplementary information') ?></h5></td>
-          <td><?php print mica_client_commons_get_localized_field($population->selectionCriteria, 'info'); ?></td>
-        </tr>
-      <?php endif; ?>
-
-      </tbody>
-    </table>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
-
 
 
 
