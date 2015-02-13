afe7cf2c7858cb3782024f4db6b3dedc1477f339
smirfolio <ben.sghaier.samir@gmail.com>
2015-02-13 15:28:13 -0500

    MK-279 Make the search result display configurable in drupal client manage Study table manage dataset table manage variable table manage Networks table + columns networks Debug hidden pagination in variable table (Dataset detail page)

--- drupal/modules/obiba_mica/obiba_mica_search/templates/obiba_mica_search_search.tpl.php
+++ drupal/modules/obiba_mica/obiba_mica_search/templates/obiba_mica_search_search.tpl.php
@@ -12,146 +12,165 @@
 
 <div id="search-result">
   <ul class="nav nav-tabs" role="tablist" id="result-search">
-    <li><a href="#networks" role="tab"> <?php print t('Networks') ?>
-        (<?php print !empty($network_total_hits) ? $network_total_hits : 0; ?>)
-      </a>
-    </li>
 
-    <li><a href="#studies" role="tab"> <?php print t('Studies') ?>
-        (<?php print !empty($study_total_hits) ? $study_total_hits : 0; ?>)
-      </a>
-    </li>
+    <?php if (!empty($search_param['search_networks'])): ?>
+      <li><a href="#networks" role="tab"> <?php print t('Networks') ?>
+          (<?php print !empty($network_total_hits) ? $network_total_hits : 0; ?>)
+        </a>
+      </li>
+    <?php endif; ?>
 
-    <li><a href="#datasets" role="tab"> <?php print t('Datasets') ?>
-        (<?php print !empty($dataset_total_hits) ? $dataset_total_hits : 0; ?>)
-      </a>
-    </li>
+    <?php if (!empty($search_param['search_studies'])): ?>
+      <li><a href="#studies" role="tab"> <?php print t('Studies') ?>
+          (<?php print !empty($study_total_hits) ? $study_total_hits : 0; ?>)
+        </a>
+      </li>
+    <?php endif; ?>
 
-    <li class="active"><a href="#variables" role="tab"><?php print t('Variables') ?>
-        (<?php print !empty($variable_total_hits) ? $variable_total_hits : 0; ?>)
-      </a>
-    </li>
+    <?php if (!empty($search_param['search_datasets'])): ?>
+      <li><a href="#datasets" role="tab"> <?php print t('Datasets') ?>
+          (<?php print !empty($dataset_total_hits) ? $dataset_total_hits : 0; ?>)
+        </a>
+      </li>
+    <?php endif; ?>
+
+    <?php if (!empty($search_param['search_variables'])): ?>
+      <li class="active"><a href="#variables" role="tab"><?php print t('Variables') ?>
+          (<?php print !empty($variable_total_hits) ? $variable_total_hits : 0; ?>)
+        </a>
+      </li>
+    <?php endif; ?>
 
   </ul>
 
   <!-- Tab panes -->
   <div class="tab-content search-result">
-    <div class="tab-pane active" id="variables">
-      <article>
-        <section>
-          <h3 class="pull-left"><?php print t('Variables') ?></h3>
 
-          <div class="pull-right lg-top-margin facet-search-form">
-            <?php print render($variable_search_form) ?>
-            <p>
-              <?php print l(t('View Coverage'), 'mica/coverage', array(
-                'attributes' => array(
-                  'class' => array(
-                    'btn',
-                    'btn-primary',
-                    'indent'
-                  )
-                ),
-                'query' => array(
-                  'query' => $query,
-                  'group-by' => 'studyIds'
-                ),
-              )); ?>
-            </p>
-          </div>
-          <div class="clearfix"/>
-          <?php print $variables_result['data']; ?>
-        </section>
-      </article>
-    </div>
+    <?php if (!empty($search_param['search_variables'])): ?>
+      <div class="tab-pane active" id="variables">
+        <article>
+          <section>
+            <h3 class="pull-left"><?php print t('Variables') ?></h3>
 
-    <div class="tab-pane" id="datasets">
-      <article>
-        <section>
-          <h3 class="pull-left"><?php print t('Datasets') ?></h3>
+            <div class="pull-right lg-top-margin facet-search-form">
+              <?php print render($variable_search_form) ?>
+              <p>
+                <?php print l(t('View Coverage'), 'mica/coverage', array(
+                  'attributes' => array(
+                    'class' => array(
+                      'btn',
+                      'btn-primary',
+                      'indent'
+                    )
+                  ),
+                  'query' => array(
+                    'query' => $query,
+                    'group-by' => 'studyIds'
+                  ),
+                )); ?>
+              </p>
+            </div>
+            <div class="clearfix"/>
+            <?php print $variables_result['data']; ?>
+          </section>
+        </article>
+      </div>
+    <?php endif; ?>
 
-          <div class="pull-right lg-top-margin facet-search-form">
-            <?php print render($dataset_search_form) ?>
-            <p>
-              <?php print l(t('View Coverage'), 'mica/coverage', array(
-                'attributes' => array(
-                  'class' => array(
-                    'btn',
-                    'btn-primary',
-                    'indent'
-                  )
-                ),
-                'query' => array(
-                  'query' => $query,
-                  'group-by' => 'datasetId'
-                ),
-              )); ?>
-            </p>
-          </div>
+    <?php if (!empty($search_param['search_datasets'])): ?>
+      <div class="tab-pane" id="datasets">
+        <article>
+          <section>
+            <h3 class="pull-left"><?php print t('Datasets') ?></h3>
 
-          <div class="clearfix"/>
-          <?php print $datasets['data']; ?>
-        </section>
-      </article>
-    </div>
+            <div class="pull-right lg-top-margin facet-search-form">
+              <?php print render($dataset_search_form) ?>
+              <p>
+                <?php print l(t('View Coverage'), 'mica/coverage', array(
+                  'attributes' => array(
+                    'class' => array(
+                      'btn',
+                      'btn-primary',
+                      'indent'
+                    )
+                  ),
+                  'query' => array(
+                    'query' => $query,
+                    'group-by' => 'datasetId'
+                  ),
+                )); ?>
+              </p>
+            </div>
 
-    <div class="tab-pane" id="studies">
-      <article>
-        <section>
-          <h3 class="pull-left"><?php print t('Studies') ?></h3>
+            <div class="clearfix"/>
+            <?php print $datasets['data']; ?>
+          </section>
+        </article>
+      </div>
+    <?php endif; ?>
 
-          <div class="pull-right lg-top-margin facet-search-form">
-            <?php print render($study_search_form) ?>
-            <p>
-              <?php print l(t('View Coverage'), 'mica/coverage', array(
-                'attributes' => array(
-                  'class' => array(
-                    'btn',
-                    'btn-primary',
-                    'indent'
-                  )
-                ),
-                'query' => array(
-                  'query' => $query,
-                  'group-by' => 'studyIds'
-                ),
-              )); ?>
-            </p>
-          </div>
-          <div class="clearfix"/>
-          <?php print $studies['data']; ?>
-        </section>
-      </article>
-    </div>
+    <?php if (!empty($search_param['search_studies'])): ?>
+      <div class="tab-pane" id="studies">
+        <article>
+          <section>
+            <h3 class="pull-left"><?php print t('Studies') ?></h3>
 
-    <div class="tab-pane" id="networks">
-      <article>
-        <section>
-          <h3 class="pull-left"><?php print t('Networks') ?></h3>
+            <div class="pull-right lg-top-margin facet-search-form">
+              <?php print render($study_search_form) ?>
+              <p>
+                <?php print l(t('View Coverage'), 'mica/coverage', array(
+                  'attributes' => array(
+                    'class' => array(
+                      'btn',
+                      'btn-primary',
+                      'indent'
+                    )
+                  ),
+                  'query' => array(
+                    'query' => $query,
+                    'group-by' => 'studyIds'
+                  ),
+                )); ?>
+              </p>
+            </div>
+            <div class="clearfix"/>
+            <?php print $studies['data']; ?>
+          </section>
+        </article>
+      </div>
+    <?php endif; ?>
 
-          <div class="pull-right lg-top-margin facet-search-form">
-            <?php print render($network_search_form) ?>
-            <p>
-              <?php print l(t('View Coverage'), 'mica/coverage', array(
-                'attributes' => array(
-                  'class' => array(
-                    'btn',
-                    'btn-primary',
-                    'indent'
-                  )
-                ),
-                'query' => array(
-                  'query' => $query,
-                  'group-by' => 'studyIds'
-                ),
-              )); ?>
-            </p>
-          </div>
-          <div class="clearfix"/>
-          <?php print $networks['data']; ?>
-        </section>
-      </article>
-    </div>
+    <?php if (!empty($search_param['search_networks'])): ?>
+      <div class="tab-pane" id="networks">
+        <article>
+          <section>
+            <h3 class="pull-left"><?php print t('Networks') ?></h3>
+
+            <div class="pull-right lg-top-margin facet-search-form">
+              <?php print render($network_search_form) ?>
+              <p>
+                <?php print l(t('View Coverage'), 'mica/coverage', array(
+                  'attributes' => array(
+                    'class' => array(
+                      'btn',
+                      'btn-primary',
+                      'indent'
+                    )
+                  ),
+                  'query' => array(
+                    'query' => $query,
+                    'group-by' => 'studyIds'
+                  ),
+                )); ?>
+              </p>
+            </div>
+            <div class="clearfix"/>
+            <?php print $networks['data']; ?>
+          </section>
+        </article>
+      </div>
+    <?php endif; ?>
 
   </div>
+
 </div>
