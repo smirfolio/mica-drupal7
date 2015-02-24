87146304ed306cbc2d4bc4a12e3d21392c1fdf78
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-02-24 16:41:01 -0500

    MK-277 - grouped taxonomies + some styling

--- drupal/modules/obiba_mica/obiba_mica_search/templates/obiba_mica_search_tab_block.tpl.php
+++ drupal/modules/obiba_mica/obiba_mica_search/templates/obiba_mica_search_tab_block.tpl.php
@@ -1,14 +1,14 @@
 <?php //dvm('search var',$studies);
 ?>
 <div id="search-facets">
-  <ul class="nav nav-tabs facets-tab" role="tablist" id="facet-search">
+  <ul class="nav nav-tabs facets-tab no-border" role="tablist" id="facet-search">
     <li class="active"><a href="#study-facet" role="tab" data-toggle="tab"> <?php print t('Studies') ?> </a></li>
     <li><a href="#variable-facet" role="tab" data-toggle="tab"><?php print t('Variables') ?> </a></li>
     <li class="pull-right nonactive monospace"><a href="" id="facets-expand-collapse">[+]</a></li>
   </ul>
 
   <!-- Tab panes -->
-  <div class="tab-content">
+  <div class="tab-content bordered-tab-content">
     <div class="tab-pane" id="variable-facet"> <?php print render($obiba_mica_variable); ?></div>
     <div class="tab-pane active" id="study-facet"> <?php print  render($obiba_mica_study); ?></div>
   </div>
