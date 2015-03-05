c55cf372e5b2102aa7220cbc05a5f87b09f49f42
Alex Prudencio <aprudencio@maelstrom-research.orgA>
2015-03-05 12:59:07 -0500

    MK-322 expand collapse link for facets and groups

--- drupal/modules/obiba_mica/obiba_mica_search/js/obiba-mica-search_collapse_deal.js
+++ drupal/modules/obiba_mica/obiba_mica_search/js/obiba-mica-search_collapse_deal.js
@@ -4,44 +4,13 @@
  */
 
 (function ($) {
-
-  var page_ready = false;
-
-  $(document).ready(function () {
-    $.cookie('activeAccordionGroup', null);
-    page_ready = true;
-  });
-
   Drupal.behaviors.obiba_mica_facet_search_collapse_block = {
     attach: function (context, settings) {
-
-      var newst = {};
-      var newst1 = {};
-
-      if (!page_ready) {
-        setupDomEventHandlers(newst);
+      if (context === document) {
         collapseAllFirstTime();
+        setupDomEventHandlers();
         return;
       }
-
-      var st = $.getCookieDataTabs('activeAccordionGroup');
-
-      $('.block-content').each(function (id, state) {
-        var current_id = this.id;
-
-        if (!current_id || current_id === "collapse-block-obiba-mica-search-facet-search") {
-          return true;
-        }
-
-        if (st[current_id]) {
-          $("#" + current_id).collapse("show");
-          newst1[current_id] = 'in';
-        }
-      });
-
-      $.saveCookieDataTabs(newst1, 'activeAccordionGroup');
-
-      updateExpandCollapsecon(newst1);
 
       /********** Show more show less on search page********/
       $('.charts', context).on('shown.bs.collapse', function () {
@@ -67,70 +36,58 @@
         });
       });
 
-      function setupDomEventHandlers(newst) {
+      function setupDomEventHandlers() {
         $('#facets-expand-collapse', context).on('click', function (e) {
           e.preventDefault();
           $(this).blur();
-          var st = $.getCookieDataTabs('activeAccordionGroup');
-          if ($.isEmptyObject(st)) {
+
+          if($(this).data('allCollapsed')) {
             expandAll();
           } else {
             collapseAll();
           }
+
+          updateExpandCollapseIcon();
         });
 
         $('.block', context).on('shown.bs.collapse', function () {
-          $(".block-content").each(function (id, state) {
-            var current_id = this.id;
-
-            if ($("#" + current_id).hasClass("in")) {
-              newst[current_id] = 'in';
-            }
-            else {
-              delete newst[current_id];
-            }
-          });
-
-          $.saveCookieDataTabs(newst, 'activeAccordionGroup');
-          updateExpandCollapsecon(newst);
+          updateExpandCollapseIcon();
         });
 
         $('.block', context).on('hidden.bs.collapse', function () {
-          $('.block-content').each(function (id, state) {
-            //var id_active = $(".panel-collapse .in").attr('id');
-            var current_id = this.id;
-
-            if ($("#" + current_id).hasClass("in")) {
-              newst[current_id] = 'in';
-            }
-            else {
-              delete newst[current_id];
-            }
-          });
-
-          $.saveCookieDataTabs(newst, 'activeAccordionGroup');
-          updateExpandCollapsecon(newst);
+          updateExpandCollapseIcon();
         });
       }
 
-      function updateExpandCollapsecon(data) {
-        $("#facets-expand-collapse").text((jQuery.isEmptyObject(data)) ? "[+]" : "[-]");
+      function updateExpandCollapseIcon(firsttime) {
+        var allCollapsed = true;
+
+        $('#search-facets .tab-pane.active .accordion-toggle').each(function () {
+          allCollapsed &= $(this).hasClass('collapsed');
+        });
+
+        $('#search-facets .tab-pane.active>section>.block-content').each(function () {
+          if ($(this).find('form.autocomplete').length && firsttime) {
+            return;
+          }
+
+          allCollapsed &= ($(this).hasClass('collapse') || $(this).hasClass('collapsing'));
+        });
+
+        $("#facets-expand-collapse").data('allCollapsed', allCollapsed).text(allCollapsed ? "[+]" : "[-]");
       }
 
       function expandAll() {
         expandGroups();
 
-        var newst1 = {};
         $(".block-content").each(function (id, state) {
           var current_id = this.id;
           if (!current_id || current_id === "collapse-block-obiba-mica-search-facet-search") return true;
           $("#" + current_id).collapse("show");
-          newst1[current_id] = 'in';
           removeCollapsedIcon(current_id, "collapsed");
         });
 
-        $.saveCookieDataTabs(newst1, 'activeAccordionGroup');
-        updateExpandCollapsecon(newst1);
+        updateExpandCollapseIcon();
       }
 
       function collapseAllFirstTime() {
@@ -146,7 +103,11 @@
 
         $(".block-content").each(function (id, state) {
           var current_id = this.id;
-          if (!current_id || current_id === "collapse-block-obiba-mica-search-facet-search") return true;
+
+          if (!current_id || current_id === "collapse-block-obiba-mica-search-facet-search") {
+            return true;
+          }
+
           if (firstTime && $("form[class='autocomplete']", $("#" + current_id)).length > 0) {
             $("#" + current_id).collapse("show");
             return true;
@@ -156,8 +117,7 @@
           addCollapsedIcon(current_id, 'collapsed');
         });
 
-        $.saveCookieDataTabs({}, 'activeAccordionGroup');
-        updateExpandCollapsecon({});
+        updateExpandCollapseIcon(firstTime);
       }
 
       function expandGroups() {
