0678a56915715a750627b7e1e14149fce2649896
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-03-23 19:30:57 -0400

    CPAC-67 - fixed tooltip in variable detail page; fixed popup on timeline

--- drupal/modules/obiba_mica/obiba_mica_dataset/js/obiba_mica_dataset_datatable_init.js
+++ drupal/modules/obiba_mica/obiba_mica_dataset/js/obiba_mica_dataset_datatable_init.js
@@ -7,92 +7,97 @@
   Drupal.behaviors.micaDataset_Datatable_init = {
 
     attach: function (context, settings) {
-      /***********AjaxTable*******************/
-      var divDataTableVar = $('#variables-table');
-      var idDataset = divDataTableVar.attr('id-dataset');
-      var typeDataset = divDataTableVar.attr('type-dataset');
-      var headerTable = null;
-      /*****************/
+      if (context === document) getAjaxTable();
 
-      $.ajax({
-        'async': false,
-        'url': Drupal.settings.basePath + 'mica/variables-tab-header/' + typeDataset,
-        'type': 'GET',
-        'success': function (data) {
-          setHeaderTab(data);
-        },
-        "dataType": "json",
-        'error': function (data) {
+      function getAjaxTable() {
+        /***********AjaxTable*******************/
+        var divDataTableVar = $('#variables-table');
+        var idDataset = divDataTableVar.attr('id-dataset');
+        var typeDataset = divDataTableVar.attr('type-dataset');
+        var headerTable = null;
+        /*****************/
 
-        }
-      });
+        $.ajax({
+          'async': false,
+          'url': Drupal.settings.basePath + 'mica/variables-tab-header/' + typeDataset,
+          'type': 'GET',
+          'success': function (data) {
+            setHeaderTab(data);
+          },
+          "dataType": "json",
+          'error': function (data) {
 
-      function setHeaderTab(data) {
-        headerTable = data["header"];
-      }
-
-      function hideSortIcon() {
-        var thHeaderSort = $(".DTFC_LeftBodyLiner .sorting");
-        var thHeaderSortAsc = $(".DTFC_LeftBodyLiner .sorting_asc");
-        var thHeaderSortDesc = $(".DTFC_LeftBodyLiner .sorting_desc");
-        if (thHeaderSort.length) {
-          thHeaderSort.removeClass("sorting");
-        }
-        if (thHeaderSortAsc.length) {
-          thHeaderSortAsc.removeClass("sorting_asc");
-        }
-        if (thHeaderSortDesc.length) {
-          thHeaderSortDesc.removeClass("sorting_desc");
-        }
-      }
-
-      /**********************/
-      if (headerTable) {
-        $("#download-btn").show();
-        var divTableVariable = $('#table-variables');
-        divTableVariable.on('xhr.dt', function (e, settings, json) {
-          if (json.iTotalRecords === 0) {
-            $(".table-variables").hide();
-          }
-          else {
-            $("#download-btn > a").on('click', function (event) {
-              // create a form for the file upload
-              var form = $("<form action='" + idDataset + '/download' + "' method='post'>");
-              $(this).after(form);
-              form.submit().remove();
-              return false;
-            });
-          }
-        });
-        var dataTable = divTableVariable.dataTable({
-          //  "scrollY": "100%",
-          "responsive": true,
-          "scrollX": true,
-          "scrollCollapse": true,
-          "processing": true,
-          //  "sDom": '<"pull-left" i><"pull-right" f>t<"pull-left" l><"pull-right" p>',
-          "sDom": '<"table-var-wrapper" <"md-top-margin" i>rt<"pull-left md-top-margin" l><"pull-right md-top-margin" p>>',
-          "bServerSide": true,
-          "sAjaxSource": Drupal.settings.basePath + 'mica/variables-tab-data/' + typeDataset + '/' + idDataset,
-          "aoColumns": headerTable,
-          "searching": true,
-          "ordering": true,
-          "iDisplayLength": 25,
-          "stateSave": true,
-          "language": {
-            "url": Drupal.settings.basePath + 'mica/datatable-international'
           }
         });
 
-        new $.fn.dataTable.FixedColumns(dataTable, {
-          leftColumns: 2
-        });
+        function setHeaderTab(data) {
+          headerTable = data["header"];
+        }
 
-        divTableVariable.on('init.dt', function () {
-          hideSortIcon();
-        });
+        function hideSortIcon() {
+          var thHeaderSort = $(".DTFC_LeftBodyLiner .sorting");
+          var thHeaderSortAsc = $(".DTFC_LeftBodyLiner .sorting_asc");
+          var thHeaderSortDesc = $(".DTFC_LeftBodyLiner .sorting_desc");
+          if (thHeaderSort.length) {
+            thHeaderSort.removeClass("sorting");
+          }
+          if (thHeaderSortAsc.length) {
+            thHeaderSortAsc.removeClass("sorting_asc");
+          }
+          if (thHeaderSortDesc.length) {
+            thHeaderSortDesc.removeClass("sorting_desc");
+          }
+        }
+
+        /**********************/
+        if (headerTable) {
+          $("#download-btn").show();
+          var divTableVariable = $('#table-variables');
+          divTableVariable.on('xhr.dt', function (e, settings, json) {
+            if (json.iTotalRecords === 0) {
+              $(".table-variables").hide();
+            }
+            else {
+              $("#download-btn > a").on('click', function (event) {
+                // create a form for the file upload
+                var form = $("<form action='" + idDataset + '/download' + "' method='post'>");
+                $(this).after(form);
+                form.submit().remove();
+                return false;
+              });
+            }
+          });
+          var dataTable = divTableVariable.dataTable({
+            //  "scrollY": "100%",
+            "responsive": true,
+            "scrollX": true,
+            "scrollCollapse": true,
+            "processing": true,
+            //  "sDom": '<"pull-left" i><"pull-right" f>t<"pull-left" l><"pull-right" p>',
+            "sDom": '<"table-var-wrapper" <"md-top-margin" i>rt<"pull-left md-top-margin" l><"pull-right md-top-margin" p>>',
+            "bServerSide": true,
+            "sAjaxSource": Drupal.settings.basePath + 'mica/variables-tab-data/' + typeDataset + '/' + idDataset,
+            "aoColumns": headerTable,
+            "searching": true,
+            "ordering": true,
+            "iDisplayLength": 25,
+            "stateSave": true,
+            "language": {
+              "url": Drupal.settings.basePath + 'mica/datatable-international'
+            }
+          });
+
+          new $.fn.dataTable.FixedColumns(dataTable, {
+            leftColumns: 2
+          });
+
+          divTableVariable.on('init.dt', function () {
+            hideSortIcon();
+          });
+
+        }
+        /*******************************/
       }
-      /*******************************/
     }
 
   }
