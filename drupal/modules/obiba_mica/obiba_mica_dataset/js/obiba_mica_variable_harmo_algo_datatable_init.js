7903dc3fc1fa70d08b6fd1ae9d7955e07645dbfb
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-03-27 17:34:23 -0400

    CPAC-80 - organized var algos in the same order as the var stat table

--- drupal/modules/obiba_mica/obiba_mica_dataset/js/obiba_mica_variable_harmo_algo_datatable_init.js
+++ drupal/modules/obiba_mica/obiba_mica_dataset/js/obiba_mica_variable_harmo_algo_datatable_init.js
@@ -7,49 +7,58 @@
   Drupal.behaviors.micaDataset_variable_harmo_algo_datable_init = {
 
     attach: function (context, settings) {
-      /**************************/
-      var TitleButtonVar = $('#harmo-algo').attr('title-button-var');
-      $('#harmo-algo').on('click', function () {
-        var idHarmonizationVariable = $(this).attr('var-id');
 
+      if (context === document) {
+        var TitleButtonVar = $('#harmo-algo').attr('title-button-var');
+        $('#harmo-algo').on('click', function () {
+          var idHarmonizationVariable = $(this).attr('var-id');
+          var sectionContainer = $('div#harmo-algo');
+          $(this).text(Drupal.t('Hide') + ' ' + TitleButtonVar);
+          var $btn = $(this).button('loading');
+          $('.collapse').collapse();
+          $.ajax({
+            'url': Drupal.settings.basePath + 'mica/variables-harmonization-algo/' + idHarmonizationVariable + '/' + JSON.stringify(getSortedVariableNames()),
+            'type': 'GET',
+            'dataType': 'html',
+            'data': '',
+            'success': function (data) {
+              try {
+                var data_decoded = jQuery.parseJSON(data);
+                sectionContainer.append(data_decoded['algo']);
+              } catch (e) {
 
-        var sectionContainer = $('div#harmo-algo');
-        $(this).text(Drupal.t('Hide') + ' ' + TitleButtonVar);
-        var $btn = $(this).button('loading');
-        $('.collapse').collapse();
-        $.ajax({
-          'url': Drupal.settings.basePath + 'mica/variables-harmonization-algo/' + idHarmonizationVariable,
-          'type': 'GET',
-          'dataType': 'html',
-          'data': '',
-          'success': function (data) {
-            try {
-              var data_decoded = jQuery.parseJSON(data);
-              sectionContainer.append(data_decoded['algo']);
-            } catch (e) {
+              }
 
+            },
+            'error': function (data) {
+              console.log('Some errors....');
             }
+          });
+          $("#harmo-algo").unbind("click");
+          $(this).removeAttr('id');
+          $(this).attr('id', 'harmo-algo-toggle');
 
-          },
-          'error': function (data) {
-            console.log('Some errors....');
-          }
+          $btn.button('reset')
         });
-        $("#harmo-algo").unbind("click");
-        $(this).removeAttr('id');
-        $(this).attr('id', 'harmo-algo-toggle');
+        $('.collapse').on('hidden.bs.collapse', function () {
 
-        $btn.button('reset')
-      });
-      $('.collapse').on('hidden.bs.collapse', function () {
+          $('#harmo-algo-toggle').text(Drupal.t('Show') + ' ' + TitleButtonVar);
+        });
+        $('.collapse').on('shown.bs.collapse', function () {
 
-        $('#harmo-algo-toggle').text(Drupal.t('Show') + ' ' + TitleButtonVar);
-      });
-      $('.collapse').on('shown.bs.collapse', function () {
+          $('#harmo-algo-toggle').text(Drupal.t('Hide') + ' ' + TitleButtonVar);
+        });
 
-        $('#harmo-algo-toggle').text(Drupal.t('Hide') + ' ' + TitleButtonVar);
-      });
-      /***************************/
+        function getSortedVariableNames() {
+          var table = $('#table-variable-harmonization').DataTable();
+          var variableNames = {};
+          $.each(table.rows().data(), function (i, cell) {
+            variableNames[cell[cell.length - 1]] = null;
+          });
+
+          return variableNames;
+        }
+      }
     }
   }
 }(jQuery));
