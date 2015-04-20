cd5b5cc4efb8804125d2d6ba8df3df574b19b3ec
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-04-20 14:45:24 -0400

    MK-369 - fixed the harmo table on nightly site

--- drupal/modules/obiba_mica/obiba_mica_dataset/js/obiba_mica_dataset_datatable_init.js
+++ drupal/modules/obiba_mica/obiba_mica_dataset/js/obiba_mica_dataset_datatable_init.js
@@ -41,61 +41,61 @@
 
     function setHeaderTab(data) {
       headerTable = data["header"];
-    }
+      if (headerTable) {
+        $("#download-btn").show();
+        var divTableVariable = $('#table-variables');
+        divTableVariable.on('xhr.dt', function (e, settings, json) {
+          if (json.iTotalRecords === 0) {
+            $(".table-variables").hide();
+          }
+          else {
+            $("#download-btn > a").on('click', function (event) {
+              // create a form for the file upload
+              var form = $("<form action='" + idDataset + '/download' + "' method='post'>");
+              $(this).after(form);
+              form.submit().remove();
+              return false;
+            });
+          }
+        });
+        var dataTable = divTableVariable.dataTable({
+          "responsive": true,
+          "scrollX": true,
+          "scrollCollapse": true,
+          "processing": true,
+          "sDom": '<"table-var-wrapper" <"md-top-margin" i>rt<"pull-left md-top-margin" l><"pull-right md-top-margin" p>>',
+          "bServerSide": true,
+          "iDisplayLength": 25,
+          "sAjaxSource": Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/variables-tab-data/' + typeDataset + '/' + idDataset,
+          "aoColumns": headerTable,
+          "searching": true,
+          "ordering": true,
+          "fnDrawCallback": function (oSettings) {
+            updateTooltip();
+          },
+          "fnInitComplete": function () {
+            updateTooltip();
+          },
+          "language": {
+            "url": Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/datatable-international'
+          }
+        });
 
-    if (headerTable) {
-      $("#download-btn").show();
-      var divTableVariable = $('#table-variables');
-      divTableVariable.on('xhr.dt', function (e, settings, json) {
-        if (json.iTotalRecords === 0) {
-          $(".table-variables").hide();
-        }
-        else {
-          $("#download-btn > a").on('click', function (event) {
-            // create a form for the file upload
-            var form = $("<form action='" + idDataset + '/download' + "' method='post'>");
-            $(this).after(form);
-            form.submit().remove();
-            return false;
+        // NOTE: this breaks the paging and table state, keep an eye for an update
+        new $.fn.dataTable.FixedColumns(dataTable, {
+          leftColumns: 2
+        });
+
+        function updateTooltip() {
+          $('span', dataTable.fnGetNodes()).tooltip({
+            "delay": 0,
+            "track": true,
+            "fade": 250
           });
         }
-      });
-      var dataTable = divTableVariable.dataTable({
-        "responsive": true,
-        "scrollX": true,
-        "scrollCollapse": true,
-        "processing": true,
-        "sDom": '<"table-var-wrapper" <"md-top-margin" i>rt<"pull-left md-top-margin" l><"pull-right md-top-margin" p>>',
-        "bServerSide": true,
-        "iDisplayLength": 25,
-        "sAjaxSource": Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/variables-tab-data/' + typeDataset + '/' + idDataset,
-        "aoColumns": headerTable,
-        "searching": true,
-        "ordering": true,
-        "fnDrawCallback": function (oSettings) {
-          updateTooltip();
-        },
-        "fnInitComplete": function () {
-          updateTooltip();
-        },
-        "language": {
-          "url": Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/datatable-international'
-        }
-      });
-
-      // NOTE: this breaks the paging and table state, keep an eye for an update
-      new $.fn.dataTable.FixedColumns(dataTable, {
-        leftColumns: 2
-      });
-
-      function updateTooltip() {
-        $('span', dataTable.fnGetNodes()).tooltip({
-          "delay": 0,
-          "track": true,
-          "fade": 250
-        });
       }
     }
+
   }
 
 }(jQuery));
