59cdb7c7d0a266c1ef127bd51af17d4996e078ca
Yannick Marcon <yannick.marcon@obiba.org>
2015-06-12 21:20:47 -0400

    User pages reviewed

--- drupal/themes/obiba_bootstrap/templates/user-login.tpl.php
+++ drupal/themes/obiba_bootstrap/templates/user-login.tpl.php
@@ -1,25 +1,10 @@
-<div class="outer">
+<p><?php print render($intro_text); ?></p>
 
-  <div class="innerdivs">
-    <div class="row">
-      <div class="col-md-6 text-center bg-primary"><?php print t('Sign In') ?></div>
-	    <?php if (!empty($form['obiba_auth']) && $form['obiba_auth']['#value'] == 'obiba_auth_user_register_form'): ?>
-		    <div class="col-md-6 text-center "><?php print l('Sign Up ', 'obiba_user/register') ?></div>
-	    <?php else: ?>
-		    <div class="col-md-6 text-center "><?php print l('Sign Up ', 'user/register') ?></div>
-	    <?php endif; ?>
-
-    </div>
-    <p><?php print render($intro_text); ?></p>
-
-    <div class="obiba-bootstrap-user-login-form-wrapper">
-      <?php print drupal_render_children($form) ?>
-    </div>
-
+<div class="row">
+  <div class="col-md-6">
+    <?php print drupal_render_children($form) ?>
     <div class="md-top-margin">
       <?php print l('Forgot your password?', 'user/password') ?>
     </div>
-
   </div>
-
 </div>
--- drupal/themes/obiba_bootstrap/templates/user-register-form.tpl.php
+++ drupal/themes/obiba_bootstrap/templates/user-register-form.tpl.php
@@ -1,20 +1,14 @@
-<div class="outer">
-  <div class="innerdivs">
-    <div class="row">
-      <div class="col-md-6 text-center "><?php print l('Sign In', 'user/login') ?></div>
-      <div class="col-md-6 text-center bg-primary"><?php print t('Sign Up') ?></div>
+<p><?php print render($intro_text); ?></p>
+
+<div class="row">
+      <div class="col-md-6">
+        <?php print drupal_render_children($form) ?>
+      </div>
     </div>
-    <p><?php print render($intro_text); ?></p>
 
     <?php if (!empty($form['obiba_auth']) && $form['obiba_auth']['#value'] == 'obiba_auth_user_register_form'): ?>
       <div class="text-center">
         <p><?php print t('Please use this following link to register') ?></p>
         <?php print l('Sign Up ', 'obiba_user/register') ?>
       </div>
-    <?php else: ?>
-      <div class="obiba-bootstrap-user-register-form-wrapper">
-        <?php print drupal_render_children($form) ?>
-      </div>
-    <?php endif; ?>
-  </div>
-</div>
+   <?php endif; ?>
