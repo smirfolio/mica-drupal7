765f97a14adcd6e92a8068fea0e478a8b7530e29
Yannick Marcon <yannick.marcon@obiba.org>
2015-06-16 14:36:03 -0400

    Attachements markup reviewed

--- drupal/modules/obiba_mica/obiba_mica_data_access_request/js/app/views/obiba_mica_data_access_request-form-page.tpl.php
+++ drupal/modules/obiba_mica/obiba_mica_data_access_request/js/app/views/obiba_mica_data_access_request-form-page.tpl.php
@@ -44,10 +44,14 @@
   <obiba-alert id="DataAccessRequestEditController"></obiba-alert>
   <form name="requestForm" ng-submit="submit(requestForm)">
     <div sf-model="form.model" sf-form="form.definition" sf-schema="form.schema" required="true"></div>
-    <h3><?php print t('Attachments'); ?></h3>
-
-    <div class="form-group md-top-margin">
-      <attachment-input files="dataAccessRequest.attachments" multiple="true"></attachment-input>
+    <h2><?php print t('Documents'); ?></h2>
+    <p><?php print t('Select one or several documents to be attached to the application form.'); ?></p>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group md-top-margin">
+          <attachment-input files="dataAccessRequest.attachments" multiple="true"></attachment-input>
+        </div>
+      </div>
     </div>
   </form>
 
