e351e7b364ce6cb8a38ade86e8ccccbb2dfbf34f
smirfolio <ben.sghaier.samir@gmail.com>
2015-08-27 11:07:52 -0400

    Profile users details in the Data access request page MK-531

--- obiba_mica_data_access_request/js/app/views/data-access-request-profile-user-modal.tpl.php
+++ obiba_mica_data_access_request/js/app/views/data-access-request-profile-user-modal.tpl.php
@@ -1,38 +1,38 @@
 <div class="modal-content">
   <div class="modal-header">
-    <button type="button" class="close" aria-hidden="true" ng-click="$dismiss()">&times;</button>
+    <button type="button" class="close" aria-hidden="true"
+      ng-click="$dismiss()">&times;</button>
     <h4 class="modal-title">
-      {{'data-access-request.profile.title' | translate}}
+      <?php print t('Applicant Profile'); ?>
     </h4>
   </div>
   <div class="modal-body">
     <div>
       <label for="recipient-name"
-        class="control-label">{{'data-access-request.profile.name' | translate}} :</label>
-      <span id="data-name-applicant">{{getFullName(dataAccessRequest.profile)}}</span>
+        class="control-label"><?php print t('Name : '); ?></label>
+      <span
+        id="data-name-applicant">{{getFullName(dataAccessRequest.profile)}}</span>
     </div>
 
     <div>
       <label for="recipient-email"
-        class="control-label">{{'data-access-request.profile.email' | translate}} :</label>
+        class="control-label"><?php print t('E-mail : '); ?></label>
       <span id="data-email-applicant">{{getProfileEmail(dataAccessRequest.profile)}}</span>
     </div>
 
-    <div ng-repeat="attribute in dataAccessRequest.profile.attributes | filterAttributes">
-      <label for="recipient-email"
-        class="control-label">{{attribute.key}}</label> :
+    <div
+      ng-repeat="attribute in dataAccessRequest.profile.attributes | filterAttributes">
+      <label for="recipient-email" class="control-label">
+        {{attribute.key}}
+      </label> :
       <span id="data-email-applicant">{{attribute.value}}</span>
     </div>
-
-    <div id="user-attributes" >
-      {{getProfileAttributes(dataAccessRequest.profile)}}
-    </div>
     <a id="data-email-applicant" class="btn btn-default" href=""
-      target="_blank"><?php print t('Send a mail to this applicant'); ?></a>
-
+      target="_blank"><?php print t('Send E-mail'); ?></a>
   </div>
   <div class="modal-footer">
-    <button type="button" class="btn btn-primary voffest4" ng-click="$dismiss()">
+    <button type="button" class="btn btn-primary voffest4"
+      ng-click="$dismiss()">
       <span ng-hide="confirm.close" translate>close</span>
       {{confirm.close}}
     </button>
