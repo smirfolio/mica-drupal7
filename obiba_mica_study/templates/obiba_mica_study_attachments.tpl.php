34fc79e63169baea4333735d6a8ee8aabae2af77
samirMtl <ben.sghaier.samir@gmail.com>
2015-10-30 12:35:29 -0400

    Sort files by name asc order

--- obiba_mica_study/templates/obiba_mica_study_attachments.tpl.php
+++ obiba_mica_study/templates/obiba_mica_study_attachments.tpl.php
@@ -4,12 +4,20 @@
     entity="study"
     id_entity="<?php print $study_id ?>"
     id_attachment="<?php print $attachment->id ?>"
-    type="<?php print $attachment->type; ?>"
     length="<?php print $attachment->size; ?>"
-    title="<?php print $attachment->fileName; ?>"
+    title="<?php print obiba_mica_commons_get_localized_field($attachment,'description'); ?>"
+    data-original-title="<?php print obiba_mica_commons_get_localized_field($attachment,'description'); ?>"
     download="<?php print rawurldecode($attachment->fileName); ?>"
+    data-placement="top"
+    data-toggle="tooltip"
     >
     <span class="glyphicon glyphicon-download"></span>
     <?php print urldecode($attachment->fileName); ?></a>
+  <?php $types =  explode(',',$attachment->type)?>
+  <div>
+  <?php foreach($types as $type): ?>
+    <span class="label label-default"><?php print $type; ?></span>
+  <?php endforeach;?>
+  </div>
 </li>
 
