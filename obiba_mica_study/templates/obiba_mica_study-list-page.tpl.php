5128718f5b52da759067a585c9398accd69de443
Yannick Marcon <yannick.marcon@obiba.org>
2015-11-17 14:21:45 -0500

    Fix study list count display

--- obiba_mica_study/templates/obiba_mica_study-list-page.tpl.php
+++ obiba_mica_study/templates/obiba_mica_study-list-page.tpl.php
@@ -3,19 +3,19 @@
 <div class="list-page">
 
   <div class="row">
-    <?php if (!empty($total_items) && $total_items >= MicaClient::getResponsePageSizeSmall()): ?>
-    <?php $count = empty($total_items) ? 0 : $total_items;
+    <?php
+    $count = empty($total_items) ? 0 : $total_items;
     $caption = $count < 2 ? t('Study') : t('Studies');
     ?>
     <div class="col-md-2 col-sm-2 col-xs-4 min-height-align search-count">
-        <span id="refresh-count">
-          <?php print $count ?>
-        </span>
-        <span id="refresh-count">
-         <?php print $caption ?>
-        </span>
+      <span id="refresh-count">
+        <?php print $count ?>
+      </span>
+      <span id="refresh-count">
+        <?php print $caption ?>
+      </span>
     </div>
-    <?php endif; ?>
+
     <div class="col-md-10  col-sm-10 col-xs-8 min-height-align pull-right">
       <div class="hidden-xs inline pull-left">
         <?php print render($form_search); ?>
