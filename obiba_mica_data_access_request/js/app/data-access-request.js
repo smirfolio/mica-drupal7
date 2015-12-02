c453dfc88e36180bae29e7033480663916ba520a
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-12-02 18:12:23 -0500

    Using the new module 'ng-obiba-mica' and removed obsolete files.

--- obiba_mica_data_access_request/js/app/data-access-request.js
+++ obiba_mica_data_access_request/js/app/data-access-request.js
@@ -8,8 +8,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * Copyright (c) 2014 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 'use strict';
-
+(function ($) {
+  Drupal.behaviors.obiba_mica_data_access_request = {
+    attach: function (context, settings) {
 
 mica.DataAccessRequest = angular.module('mica.DataAccessRequest', [
   'obiba.mica.access'
