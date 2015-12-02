426a7a509eaac4328937296467a4a6bff10d0ce4
samirMtl <ben.sghaier.samir@gmail.com>
2015-12-02 11:33:12 -0500

    Mk-613 Variables are not displayed in harmonization dataset

--- obiba_mica_dataset/js/obiba_mica_dataset_datatable_init.js
+++ obiba_mica_dataset/js/obiba_mica_dataset_datatable_init.js
@@ -22,13 +22,13 @@
 
   function createDataTable() {
     var divDataTableVar = $('#variables-table');
-    var idDataset = divDataTableVar.attr('id-dataset');
     var typeDataset = divDataTableVar.attr('type-dataset');
+    var idDataset =Drupal.settings.datasetId;
     var headerTable = null;
 
     $.ajax({
       'async': false,
-      'url': Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/variables-tab-header/' + typeDataset,
+      'url': Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/variables-tab-header/' + typeDataset + '/' + idDataset,
       'type': 'GET',
       'success': function (data) {
         setHeaderTab(data);
