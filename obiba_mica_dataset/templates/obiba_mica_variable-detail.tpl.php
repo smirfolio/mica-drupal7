6f3e1a05af1517658b20d72801175ff288a85d97
Samir ben sghaier <sbensghaier@maelstrom-research.org>
2016-05-26 11:14:52 -0400

    MLSTRM-398

--- obiba_mica_dataset/templates/obiba_mica_variable-detail.tpl.php
+++ obiba_mica_dataset/templates/obiba_mica_variable-detail.tpl.php
@@ -195,7 +195,7 @@
       <?php
       $column_for_detail_statistics = 6;
       if (!variable_get_value('dataset_detailed_var_stats') && $variable_dto->variableType == 'Dataschema') : ?>
-        <p><?php print t('Cumulative Summary of all Atudies:') ?></p>
+        <p><?php print t('Cumulative Summary of all Studies:') ?></p>
       <?php endif; ?>
       <?php if ($variable_dto->variableType == 'Dataschema' && variable_get_value('dataset_detailed_var_stats')): ?>
         <div class="scroll-content-tab">
