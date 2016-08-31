ad7b86db90771e777f1a41e9ac81fac8f5382ba2
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2016-08-31 15:15:34 -0400

    Added BS style to hide sections when printing.

---
+++ js/print-friendly.js
@@ -0,0 +1,8 @@
+(function ($) {
+  $(document).ready(function() {
+    $("div.main-container ol.breadcrumb").addClass( 'hidden-print' );
+    $('div.main-container h1.page-header').addClass( 'hidden-print' );
+    $('div.alert').addClass( 'hidden-print' );
+    $('footer').addClass( 'hidden-print' );
+  });
+}(jQuery));
