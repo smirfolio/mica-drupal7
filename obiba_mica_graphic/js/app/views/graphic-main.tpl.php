838cf745ce265aca12fc608334a6435f7e00a5ca
Cedric Fontin <cfontin@maelstrom-research.org>
2016-10-06 13:09:09 -0400

    MLSTRM-421 - remove useless selectTab function calls from graphics template

--- obiba_mica_graphic/js/app/views/graphic-main.tpl.php
+++ obiba_mica_graphic/js/app/views/graphic-main.tpl.php
@@ -1,20 +1,16 @@
 <div>
   <uib-tabset active="activeTab" class="voffset5">
 
-    <uib-tab ng-click="selectTab('geoCharts')"
-             heading="{{'graphics.geo-charts' | translate}}">
+    <uib-tab heading="{{'graphics.geo-charts' | translate}}">
     </uib-tab>
     
-    <uib-tab ng-click="selectTab('studyDesign')"
-             heading="{{'graphics.study-design' | translate}}">
+    <uib-tab heading="{{'graphics.study-design' | translate}}">
     </uib-tab>
     
-    <uib-tab ng-click="selectTab('nbrParticipants')"
-             heading="{{'graphics.number-participants' | translate}}">
+    <uib-tab heading="{{'graphics.number-participants' | translate}}">
     </uib-tab>
     
-    <uib-tab ng-click="selectTab('bioSamples')"
-             heading="{{'graphics.bio-samples' | translate}}">
+    <uib-tab heading="{{'graphics.bio-samples' | translate}}">
     </uib-tab>
     
   </uib-tabset>
