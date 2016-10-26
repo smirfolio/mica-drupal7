24806040619f07451bc651403085b30851ee50b2
Cedric Fontin <cfontin@maelstrom-research.org>
2016-10-26 15:03:31 -0400

    MK-1106 - add network tables to dataschema page details

--- obiba_mica_dataset/templates/obiba_mica_dataset-detail.tpl.php
+++ obiba_mica_dataset/templates/obiba_mica_dataset-detail.tpl.php
@@ -121,9 +121,25 @@ $description = empty($dataset_dto->description) ? NULL : obiba_mica_commons_get_
     </section>
   <?php endif; ?>
 
-  <!-- STUDIES -->
-  <?php if (variable_get_value('dataset_show_studies') && ($dataset_type == "study-dataset" || !empty($dataset_type_dto->studyTables))): ?>
-    <section>
+
+  <section>
+    <!-- NETWORKS -->
+    <?php if (!empty($dataset_type_dto->networkTables)): ?>
+      <div>
+        <h2><?php print variable_get_value('networks_section_label') ?></h2>
+
+        <div id="networks-table">
+          <div class="row">
+            <div class="col-lg-12 col-xs-12">
+              <table class="table table-striped" id="table-networks"></table>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <!-- STUDIES -->
+    <?php if (variable_get_value('dataset_show_studies') && ($dataset_type == "study-dataset" || !empty($dataset_type_dto->studyTables))): ?>
       <h2>
         <?php
         if (!empty($dataset_type_dto->project)):
@@ -218,8 +234,8 @@ $description = empty($dataset_dto->description) ? NULL : obiba_mica_commons_get_
           </div>
         </div>
       <?php endif ?>
-    </section>
-  <?php endif ?>
+    <?php endif ?>
+  </section>
 
   <!-- COVERAGE -->
   <?php if (!empty($coverage)): ?>
