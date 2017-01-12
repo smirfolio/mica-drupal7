f5dcc6cc6335a028599aa57e93a53ae47e9fd626
Cedric Fontin <cfontin@maelstrom-research.org>
2017-01-12 12:25:43 -0500

    MK-1240 - use localized error for when no crosstab data
    Conflicts:
    	obiba_mica_dataset/js/app/dataset-variable-crosstab-controller.js

--- obiba_mica_dataset/js/app/dataset-variable-crosstab-controller.js
+++ obiba_mica_dataset/js/app/dataset-variable-crosstab-controller.js
@@ -70,8 +70,7 @@
 
             var onError = function (response) {
               $scope.serverError = true;
-              ServerErrorAlertService.alert('DataAccessRequestEditController', response);
-              ForbiddenDrupalRedirect.redirectDrupalMessage(response);
+              ServerErrorAlertService.alert('MainController', response);
               endProgress();
             };
 
@@ -325,10 +324,15 @@
                     v2: $scope.crosstab.rhs.xVariable.name
                   },
                   function onSuccess(response) {
-                    $scope.crosstab.contingencies = normalizeData(response.contingencies ? response.contingencies : [response]);
+                    if (Object.keys(response).filter(function (k) {return k[0] !== '$';}).length === 0) {
+                      // response with all properties prefixed with '$' filtered out is empty
+                      onError({status: 'crosstab.no-data'});
+                    } else {
+                      $scope.crosstab.contingencies = normalizeData(response.contingencies ? response.contingencies : [response]);
 
-                    if ($scope.datasetHarmo) {
-                      $scope.crosstab.all = normalizeData(response.all ? [response.all] : [])[0];
+                      if ($scope.datasetHarmo) {
+                        $scope.crosstab.all = normalizeData(response.all ? [response.all] : [])[0];
+                      }
                     }
 
                     endProgress();
