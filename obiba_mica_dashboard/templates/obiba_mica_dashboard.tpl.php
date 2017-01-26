1a6d32d47de184d288f210a42d7008d9411b3fac
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2017-01-26 16:46:19 -0500

    MK-1042 - added a module to show metrics in a block.

---
+++ obiba_mica_dashboard/templates/obiba_mica_dashboard.tpl.php
@@ -0,0 +1,24 @@
+<?php ?>
+
+<div>
+  <h1>
+    <span class="label label-info"><?php print $metrics['Network']->title ?>
+      <span class="badge"><?php print $metrics['Network']->published ?></span>
+    </span>
+  </h1>
+  <h1>
+    <span class="label label-warning"><?php print $metrics['Study']->title ?>
+      <span class="badge"><?php print $metrics['Study']->published ?></span>
+    </span>
+  </h1>
+  <h1>
+    <span class="label label-success"><?php print $metrics['StudyWithVariable']->title ?>
+      <span class="badge"><?php print $metrics['StudyWithVariable']->published ?></span>
+    </span>
+  </h1>
+  <h1>
+    <span class="label label-default"><?php print $metrics['DatasetVariable']->title ?>
+      <span class="badge"><?php print $metrics['DatasetVariable']->published ?></span>
+    </span>
+  </h1>
+</div>
