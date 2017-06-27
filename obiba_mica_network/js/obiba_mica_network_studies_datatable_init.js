f978ef63a8193d78253d8723ddd890835a16e130
samirMtl <ben.sghaier.samir@gmail.com>
2017-06-27 12:47:42 -0400

    WIP Collection/harmonization Studies list in Network detail page Facet List : Update the link to detail study

--- obiba_mica_network/js/obiba_mica_network_studies_datatable_init.js
+++ obiba_mica_network/js/obiba_mica_network_studies_datatable_init.js
@@ -13,29 +13,43 @@
 
     attach: function (context, settings) {
       if (context === document) {
-        $('#table-studies').dataTable(
-          {
-            "bAutoWidth": false,
-            "responsive": true,
-            "data": settings.table_data,
-            "columns": settings.table_headers,
-            "iDisplayLength": 25,
-            "scrollCollapse": true,
-            "sDom": '<"table-var-wrapper" <<"md-top-margin pull-left" i><"pull-right" f>><"clear-fix" ><"no-float-tab" r<"scroll-content-tab" t>><"clear-fix" ><"pull-left md-top-margin" l><"pull-right md-top-margin" p>>',
-            "language": {
-              "url": Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/datatable-international'
-            },
-            "fnDrawCallback": function (oSettings) {
-              if (oSettings._iDisplayLength > oSettings.aoData.length) {
-                $('.dataTables_paginate').hide();
-                $('.dataTables_length').hide();
-              } else {
-                $('.dataTables_paginate').show();
-                $('.dataTables_length').show();
+        if(settings.collection_study_table_data){
+          buildTable('#table-collection-studies', settings.collection_study_table_data, settings.collection_study_table_headers);
+        }
+        else{
+        $("#collection-studies-table").remove();
+        }
+        if(settings.harmonization_study_table_data) {
+          buildTable('#table-harmo-studies', settings.harmonization_study_table_data, settings.harmonization_study_table_headers);
+        }
+        else{
+          $("#harmonization-studies-table").remove()
+        }
+        function buildTable(idTable, data, header){
+          $(idTable).dataTable(
+            {
+              "bAutoWidth": false,
+              "responsive": true,
+              "data": data,
+              "columns": header,
+              "iDisplayLength": 25,
+              "scrollCollapse": true,
+              "sDom": '<"table-var-wrapper" <<"md-top-margin pull-left" i><"pull-right" f>><"clear-fix" ><"no-float-tab" r<"scroll-content-tab" t>><"clear-fix" ><"pull-left md-top-margin" l><"pull-right md-top-margin" p>>',
+              "language": {
+                "url": Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/datatable-international'
+              },
+              "fnDrawCallback": function (oSettings) {
+                if (oSettings._iDisplayLength > oSettings.aoData.length) {
+                  $('.dataTables_paginate').hide();
+                  $('.dataTables_length').hide();
+                } else {
+                  $('.dataTables_paginate').show();
+                  $('.dataTables_length').show();
+                }
               }
             }
-          }
-        );
+          );
+        }
       }
     }
   }
