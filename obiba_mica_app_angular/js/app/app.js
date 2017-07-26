9cc300ae5247e755a641eec646c85a6e4769ca91
Romain Roces <romain.roces@gmail.com>
2017-07-26 12:19:52 -0400

    MK-1451 Fix studyPopulations link in search page

--- obiba_mica_app_angular/js/app/app.js
+++ obiba_mica_app_angular/js/app/app.js
@@ -94,7 +94,7 @@ mica.config(['ngObibaMicaSearchProvider', 'ngObibaMicaUrlProvider',
     ngObibaMicaUrlProvider.setUrl('VariablePage', Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/variable/:variable');
     ngObibaMicaUrlProvider.setUrl('NetworkPage', Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/network/:network');
     ngObibaMicaUrlProvider.setUrl('StudyPage', Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/:type/:study');
-    ngObibaMicaUrlProvider.setUrl('StudyPopulationsPage', Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/study/:study/#population-:population');
+    ngObibaMicaUrlProvider.setUrl('StudyPopulationsPage', Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/:type/:study/#population-:population');
     ngObibaMicaUrlProvider.setUrl('DatasetPage', Drupal.settings.basePath + Drupal.settings.pathPrefix + 'mica/:type/:dataset');
     ngObibaMicaUrlProvider.setUrl('BaseUrl', Drupal.settings.basePath + Drupal.settings.pathPrefix);
     ngObibaMicaUrlProvider.setUrl('FileBrowserFileResource', Drupal.settings.basePath + 'mica/file');
