aa89e315e0667a41b22ad4ea2a2de813cdd6dfc3
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2019-03-21 18:13:25 -0400

    Changed the order of Harmonizarion legent: Complete/Incomplete/Undetermined (#1637)

--- obiba_mica_dataset/templates/obiba_mica_dataset-harmonization-table-legend.tpl.php
+++ obiba_mica_dataset/templates/obiba_mica_dataset-harmonization-table-legend.tpl.php
@@ -24,11 +24,6 @@
     </p>
   <?php endif ?>
   <div>
-    <i class="glyphicon glyphicon-question-sign alert-warning"></i>
-    <h6><?php print $localize->getTranslation('client.label.dataset.undetermined') ?></h6>
-    <?php print ' - ' . variable_get_value('dataset_harmonization_undetermined_description') ?>
-  </div>
-  <div>
     <i class="glyphicon glyphicon-ok alert-success"></i>
     <h6><?php print $localize->getTranslation('client.label.dataset.complete') ?></h6>
     <?php print ' - ' . variable_get_value('dataset_harmonization_complete_description') ?>
@@ -37,5 +32,10 @@
     <i class="glyphicon <?php print ObibaDatasetConstants::getIcon(); ?> "></i>
     <h6><?php print variable_get_value('dataset_harmonization_impossible_label'); ?></h6>
     <?php print ' - ' . variable_get_value('dataset_harmonization_impossible_description') ?>
+  </div>
+  <div>
+    <i class="glyphicon glyphicon-question-sign alert-warning"></i>
+    <h6><?php print $localize->getTranslation('client.label.dataset.undetermined') ?></h6>
+    <?php print ' - ' . variable_get_value('dataset_harmonization_undetermined_description') ?>
   </div>
 </div>
