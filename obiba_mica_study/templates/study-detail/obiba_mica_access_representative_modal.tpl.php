ccc0915ac6828e1ca52c4605e834cb7278c4dd65
samirMr <sbensghaier@maelstrom-research.org>
2019-09-16 16:01:16 -0400

    Add title and university for study representative Fix xss vulnerability

--- obiba_mica_study/templates/study-detail/obiba_mica_access_representative_modal.tpl.php
+++ obiba_mica_study/templates/study-detail/obiba_mica_access_representative_modal.tpl.php
@@ -4,8 +4,8 @@
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
         <h3 class="modal-title">
-          <?php print !empty($person->title)?filter_xss($person->title, obiba_mica_commons_allowed_filter_xss_tags()):''; ?>
-          <?php print !empty($person->name)?filter_xss($person->name, obiba_mica_commons_allowed_filter_xss_tags()):''; ?>
+          <?php print !empty($person->title)?obiba_mica_commons_get_localized_field($person, 'title'):''; ?>
+          <?php print !empty($person->name)?obiba_mica_commons_get_localized_field($person, 'name'):''; ?>
         </h3>
       </div>
       <div class="modal-body" test-ref="modal-body">
@@ -17,14 +17,14 @@
               <?php if (!empty($person->email)): ?>
                 <tr>
                   <th><?php print $localize->getTranslation('contact.email') ?></th>
-                  <td test-ref="email"><p><?php print $person->email; ?></p></td>
+                  <td test-ref="email"><p><?php print obiba_mica_commons_get_localized_field($person, 'email'); ?></p></td>
                 </tr>
               <?php endif; ?>
 
               <?php if (!empty($person->telephone)): ?>
                 <tr>
                   <th><?php print $localize->getTranslation('contact.phone') ?></th>
-                  <td test-ref="phone"><p><?php print $person->telephone; ?></p></td>
+                  <td test-ref="phone"><p><?php print obiba_mica_commons_get_localized_field($person,'telephone'); ?></p></td>
                 </tr>
               <?php endif; ?>
               <?php if (!empty($person->institution)): ?>
@@ -32,7 +32,7 @@
                       <th><?php print $localize->getTranslation('contact.institution') ?></th>
                       <td test-ref="institutionIdentifier">
                           <p>
-                            <?php print $person->institution; ?>
+                            <?php print obiba_mica_commons_get_localized_field($person, 'institution'); ?>
                           </p>
                       </td>
                   </tr>
