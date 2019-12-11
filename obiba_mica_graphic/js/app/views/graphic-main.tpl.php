92bc15fb0575a09f1079de66974582ddcdb517bc
samirMr <sbensghaier@maelstrom-research.org>
2019-12-11 16:36:34 -0500

    Make sure to display graphics for only existing Taxonomy

--- obiba_mica_graphic/js/app/views/graphic-main.tpl.php
+++ obiba_mica_graphic/js/app/views/graphic-main.tpl.php
@@ -1,18 +1,18 @@
 <div>
   <uib-tabset active="activeTab" class="voffset5">
 
-    <uib-tab heading="{{'graphics.geo-charts' | translate}}">
+    <uib-tab heading="{{'graphics.geo-charts' | translate}}" ng-show="getVocabulary('populations-selectionCriteria-countriesIso')">
     </uib-tab>
     
-    <uib-tab heading="{{'graphics.study-design' | translate}}">
+    <uib-tab heading="{{'graphics.study-design' | translate}}" ng-show="getVocabulary('methods-design')">
     </uib-tab>
     
-    <uib-tab heading="{{'graphics.number-participants' | translate}}">
+    <uib-tab heading="{{'graphics.number-participants' | translate}}" ng-show="getVocabulary('numberOfParticipants-participant-range')">
     </uib-tab>
     
-    <uib-tab heading="{{'graphics.bio-samples' | translate}}">
+    <uib-tab heading="{{'graphics.bio-samples' | translate}}" ng-show="getVocabulary('populations-dataCollectionEvents-bioSamples')">
     </uib-tab>
-    <uib-tab heading="{{'graphics.study-start-year' | translate}}">
+    <uib-tab heading="{{'graphics.study-start-year' | translate}}" ng-show="getVocabulary('start-range')">
     </uib-tab>
     
   </uib-tabset>
