95f698a1d365a1ed79fed6cee2838e92589877c5
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2020-05-25 14:26:55 -0400

    Changed client to use the new metrics DTO.

--- obiba_mica_dashboard/templates/obiba_mica_dashboard.tpl.php
+++ obiba_mica_dashboard/templates/obiba_mica_dashboard.tpl.php
@@ -3,22 +3,22 @@
 <div>
   <h1>
     <span class="label label-info"><?php print $metrics['Network']->title ?>
-      <span class="badge"><?php print $metrics['Network']->published ?></span>
+      <span class="badge"><?php print $metrics['Network']->publishedFormatted ?></span>
     </span>
   </h1>
   <h1>
     <span class="label label-warning"><?php print $metrics['Study']->title ?>
-      <span class="badge"><?php print $metrics['Study']->published ?></span>
+      <span class="badge"><?php print $metrics['Study']->publishedFormatted ?></span>
     </span>
   </h1>
   <h1>
     <span class="label label-success"><?php print $metrics['HarmonizationStudy']->title ?>
-      <span class="badge"><?php print $metrics['HarmonizationStudy']->published ?></span>
+      <span class="badge"><?php print $metrics['HarmonizationStudy']->publishedFormatted ?></span>
     </span>
   </h1>
   <h1>
     <span class="label label-default"><?php print $metrics['DatasetVariable']->title ?>
-      <span class="badge"><?php print $metrics['DatasetVariable']->published ?></span>
+      <span class="badge"><?php print $metrics['DatasetVariable']->publishedFormatted ?></span>
     </span>
   </h1>
 </div>
